73b64234a5a61f1742fc1cbff45e6ccae40b3ecb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-09-10 15:48:05 +0700

    Using single state FFT, clean up

--- gamma_only/calc_rhoe_gamma.jl
+++ gamma_only/calc_rhoe_gamma.jl
@@ -15,42 +15,33 @@ function calc_rhoe!(
     Npoints = prod(Ns)
     Nstates = size(psis.data[1],2)
 
-    psiR = zeros(ComplexF64, Npoints, Nstates)
+    psiR = zeros(ComplexF64, Npoints)
 
     # dont forget to zero out the Rhoe first
-    Rhoe[:,:] .= 0.0
+    fill!(Rhoe, 0.0)
 
     idx_gw2r = pw.gvecw.idx_gw2r
     idx_gw2rm = pw.gvecw.idx_gw2rm
 
     for ispin = 1:Nspin
-
-        psiR .= 0.0 + im*0.0
-        
         # Transform to real space
         for ist in 1:Nstates
-            psiR[1,ist] = psis.data[ispin][1,ist]
+            fill!(psiR, 0.0 + im*0.0)
+            psiR[1] = psis.data[ispin][1,ist]
             for igw in 2:Ngw
                 ip = idx_gw2r[igw]
                 ipm = idx_gw2rm[igw]
-                psiR[ip,ist] = psis.data[ispin][igw,ist]
-                psiR[ipm,ist] = conj(psis.data[ispin][igw,ist])
+                psiR[ip] = psis.data[ispin][igw,ist]
+                psiR[ipm] = conj(psis.data[ispin][igw,ist])
             end
-        end
-        
-        G_to_R!(pw, psiR)
-
-        # orthonormalization in real space
-        #ortho_sqrt!( psiR )
-        ##ortho_gram_schmidt!( psiR ) # for comparison
-        #psiR = sqrt(Npoints/CellVolume)*psiR
-        
-        # by pass orthonormalization, only use scaling
-        psiR = sqrt(Npoints/CellVolume)*sqrt(Npoints)*psiR
-
-        for ist = 1:Nstates, ip = 1:Npoints
-            Rhoe[ip,ispin] = Rhoe[ip,ispin] + Focc[ist,ispin]*real( conj(psiR[ip,ist])*psiR[ip,ist] )
-        end
+            G_to_R!(pw, psiR)
+            # by pass orthonormalization, only use scaling
+            psiR = sqrt(Npoints/CellVolume)*sqrt(Npoints)*psiR
+            #
+            for ip = 1:Npoints
+                Rhoe[ip,ispin] = Rhoe[ip,ispin] + Focc[ist,ispin]*real( conj(psiR[ip])*psiR[ip] )
+            end
+        end # ist
     end # ispin
 
     # Ensure that there is no negative rhoe
