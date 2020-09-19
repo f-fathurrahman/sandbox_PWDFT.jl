a7f29d6cdec5bd5e9b74e4bc3bd08ed2d66ce533
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-09-19 10:59:14 +0700

    Test CURAND

--- cuda_01/test/test_rand.jl
+++ cuda_01/test/test_rand.jl
@@ -1,8 +1,8 @@
 using Random
-using CuArrays
+using CUDA
 
 Random.seed!(1234)
-CuArrays.CURAND.seed!(1234)
+CUDA.CURAND.seed!(1234)
 
 function gen_rand()
     v = rand(ComplexF64,2,2)
@@ -10,13 +10,13 @@ function gen_rand()
 end
 
 function cu_gen_rand()
-    v = CuArrays.rand(ComplexF64,2,2)
+    v = CUDA.CURAND.rand(ComplexF64,2,2)
     return v
 end
 
 function main()
     v_cpu = gen_rand()
-    v_gpu = gen_rand()
+    v_gpu = cu_gen_rand()
 
     println(v_cpu)
     println(v_gpu)
