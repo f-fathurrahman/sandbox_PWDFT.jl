6f367563401ae486708bf471f22c91b36d211bbd
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-09-21 08:36:37 +0700

    Rename files

--- gamma_only/main_calc_forces.jl
+++ gamma_only/main_calc_forces.jl
@@ -9,39 +9,7 @@ const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
 const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
 const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
 
-include("PWGridGamma.jl")
-include("wrappers_fft_gamma.jl")
-include("ortho_GS_gamma.jl")
-include("PsPotNLGamma.jl")
-include("HamiltonianGamma.jl")
-include("BlochWavefuncGamma.jl")
-include("calc_rhoe_gamma.jl")
-include("Poisson_solve_gamma.jl")
-include("op_K_gamma.jl")
-include("op_V_loc_gamma.jl")
-include("op_V_Ps_nloc_gamma.jl")
-include("op_H_gamma.jl")
-include("calc_energies_gamma.jl")
-include("calc_grad_gamma.jl")
-
-include("setup_guess_wavefunc.jl")
-
-include("KS_solve_Emin_PCG_dot.jl")
-include("calc_energies_grad.jl")
-include("linmin_grad.jl")
-
-include("KS_solve_Emin_PCG_dot_gamma.jl")
-include("calc_energies_grad_gamma.jl")
-include("linmin_grad_gamma.jl")
-
-include("unfold_BlochWavefuncGamma.jl")
-
-include("../get_default_psp.jl")
-
-include("calc_forces_Ps_loc_gamma.jl")
-include("calc_forces_Ps_nloc_gamma.jl")
-include("calc_forces_NN_gamma.jl")
-include("calc_forces_gamma.jl")
+include("INC_gamma_only.jl")
 
 function main(molname)
 
