b854771176232cecc88b075665bd5773164361d1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-09-22 10:39:09 +0700

    Test diag_Emin_PCG_gamma

---
+++ gamma_only/test_diag.jl
@@ -0,0 +1,54 @@
+using Printf
+using LinearAlgebra
+using Random
+using FFTW
+
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+include("INC_gamma_only.jl")
+include("diag_Emin_PCG_gamma.jl")
+
+function do_calc(molname; gamma_only=true)
+
+    Random.seed!(1234)
+
+    filename = joinpath(DIR_STRUCTURES, "DATA_G2_mols", molname*".xyz")
+    atoms = Atoms(ext_xyz_file=filename)
+    pspfiles = get_default_psp(atoms)
+    
+    ecutwfc = 15.0
+    
+    Ham = HamiltonianGamma(atoms, pspfiles, ecutwfc)
+    psis = randn_BlochWavefuncGamma(Ham)
+
+    if gamma_only
+        evals = diag_Emin_PCG!(Ham, psis, verbose=true)
+    else
+        Ham_ = Hamiltonian( atoms, pspfiles, ecutwfc, use_symmetry=false )
+        psiks = unfold_BlochWavefuncGamma( Ham.pw, Ham_.pw, psis )
+        evals = diag_Emin_PCG!(Ham_, psiks, verbose=true)
+    end
+
+
+
+end
+
+
+function main()
+    Nargs = length(ARGS)
+    if Nargs >= 1
+        molname = ARGS[1]
+    else
+        molname = "H2O"
+    end
+    for i in 1:1
+        do_calc(molname, gamma_only=true)
+        do_calc(molname, gamma_only=false)
+    end
+end
+
+main()
