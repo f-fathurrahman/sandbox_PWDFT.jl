2e20021fcf2b3f5615c4673583aa394d32b9194e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-11-09 11:06:02 +0700

    Test md with gamma only

--- gamma_only/main_verlet_01.jl
+++ gamma_only/main_verlet_01.jl
@@ -1,17 +1,16 @@
 using Printf
 using Random
 using LinearAlgebra
+using FFTW
 
 using PWDFT
 
 const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)), "..")
 const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
 
 include(joinpath(DIR_PWDFT, "utilities", "PWSCF.jl"))
-
-include("../NLopt_v3/calc_energies_grad.jl")
-include("../NLopt_v3/KS_solve_Emin_PCG_new.jl")
-include("../NLopt_v3/KS_solve_Emin_PCG_vec.jl")
+include("INC_gamma_only.jl")
 include("update_positions.jl")
 
 Random.seed!(1234)
@@ -22,14 +21,11 @@ const ELECTRONMASS_SI = 9.10938215e-31
 const AMU_AU = AMU_SI / ELECTRONMASS_SI
 
 function init_Ham_H2O()
-    # Atoms
-    atoms = Atoms( ext_xyz_file="H2O.xyz" )
-
-    # Initialize Hamiltonian
     ecutwfc = 15.0
-    pspfiles = [joinpath(DIR_PSP, "O-q6.gth"),
-                joinpath(DIR_PSP, "H-q1.gth")]
-    Ham = Hamiltonian( atoms, pspfiles, ecutwfc )
+    filename = joinpath(DIR_STRUCTURES, "DATA_G2_mols", "H2O.xyz")
+    atoms = Atoms(ext_xyz_file=filename)
+    pspfiles = get_default_psp(atoms)
+    Ham = HamiltonianGamma( atoms, pspfiles, ecutwfc )
     # Set masses
     Ham.atoms.masses[:] = [16.0, 2.0]*AMU_AU
 
@@ -38,14 +34,12 @@ end
 
 
 function init_Ham_CO2()
-    # Atoms
-    atoms = Atoms( ext_xyz_file="CO2.xyz" )
-
-    # Initialize Hamiltonian
     ecutwfc = 15.0
-    pspfiles = [joinpath(DIR_PSP, "C-q4.gth"),
-                joinpath(DIR_PSP, "O-q6.gth")]
-    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, use_symmetry=false )
+    #filename = joinpath(DIR_STRUCTURES, "DATA_G2_mols", "CO2.xyz")
+    filename = joinpath("../md_01/CO2.xyz")
+    atoms = Atoms(ext_xyz_file=filename)
+    pspfiles = get_default_psp(atoms)
+    Ham = HamiltonianGamma( atoms, pspfiles, ecutwfc )
     # Set masses
     Ham.atoms.masses[:] = [14.0, 16.0]*AMU_AU
 
@@ -64,17 +58,9 @@ function run_pwscf( Ham )
     return pwscf_energies, pwscf_forces
 end
 
-function run_pwdft_jl( Ham )
-    psiks = rand_BlochWavefunc(Ham)
-    #KS_solve_Emin_PCG!( Ham, psiks )
-    KS_solve_Emin_PCG_vec!( Ham, psiks )
-    forces = calc_forces( Ham, psiks )
-    return sum(Ham.energies), forces
-end
-
-function run_pwdft_jl!( Ham, psiks )
-    KS_solve_Emin_PCG_vec!( Ham, psiks, skip_initial_diag=true, etot_conv_thr=1e-8 )
-    forces = calc_forces( Ham, psiks )
+function run_pwdft_jl!( Ham, psis )
+    KS_solve_Emin_PCG_dot!( Ham, psis, skip_initial_diag=true, etot_conv_thr=1e-8 )
+    forces = calc_forces( Ham, psis )
     return sum(Ham.energies), forces
 end
 
@@ -86,8 +72,9 @@ function main( init_func; fnametrj="TRAJ.xyz", fnameetot="ETOT.dat" )
 
     Natoms = Ham.atoms.Natoms
 
-    psiks = rand_BlochWavefunc(Ham)
-    energies, forces = run_pwdft_jl!(Ham, psiks)
+    psis = randn_BlochWavefuncGamma(Ham)
+    energies, forces = run_pwdft_jl!(Ham, psis)
+
     Etot = sum(energies)
 
     println("Initial r  =")
@@ -119,7 +106,7 @@ function main( init_func; fnametrj="TRAJ.xyz", fnameetot="ETOT.dat" )
     #
     # Start MD loop here
     #
-    NiterMax = 10
+    NiterMax = 500
     for iter = 1:NiterMax
 
         @printf(filetraj, "%d  Etot_conserved = %18.10f\n\n", Natoms, Etot_conserved)
@@ -147,9 +134,9 @@ function main( init_func; fnametrj="TRAJ.xyz", fnameetot="ETOT.dat" )
             Ekin_ions = Ekin_ions + 0.5*m[isp]*ptot2
         end
         Etot_conserved = Etot + Ekin_ions
-        update_positions!( Ham, dr )
+        update_positions!( Ham, Ham.atoms.positions + dr )
 
-        energies, forces[:] = run_pwdft_jl!(Ham, psiks)
+        energies, forces[:] = run_pwdft_jl!(Ham, psis)
         Etot = sum(energies)
 
         for ia in 1:Natoms
