8e5a9416970cc5d3e5b7177fe156e964e7d5dbb9
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-11-19 03:36:43 +0700

    Energy diff in meV

--- gamma_only/plot_Etot.jl
+++ gamma_only/plot_Etot.jl
@@ -4,15 +4,17 @@ import PyPlot
 const plt = PyPlot
 
 function main(prefix::String)
+    Ha2eV = 27.211386018
     data = readdlm(prefix)
     Ndata = size(data,1)
     @views Etot = data[2:Ndata,2]
     Etot0 = Etot[1]
     plt.clf()
     #plt.plot(data[2:Ndata,1], Etot .- Etot0, marker="o")
-    plt.plot(data[2:Ndata,1], Etot .- Etot0)
+    plt.plot(data[2:Ndata,1], (Etot .- Etot0)*Ha2eV*1000)
     plt.grid()
     plt.title(prefix)
+    plt.ylabel("meV")
     plt.savefig("IMG_"*prefix*".pdf")
 end
 
