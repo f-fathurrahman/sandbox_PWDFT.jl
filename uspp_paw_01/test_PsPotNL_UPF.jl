16c2ace98bb4908a283c80531c086f5ed16298fc
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-16 15:02:23 +0700

    Create pspots from atoms for UPF HGH

--- uspp_paw_01/test_PsPotNL_UPF.jl
+++ uspp_paw_01/test_PsPotNL_UPF.jl
@@ -9,23 +9,36 @@ using PWDFT
 const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
 const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures/DATA_G2_mols")
 
+const UPF_HGH_DIR = "/home/efefer/pseudo/HGH"
+
 include("PsPot_UPF.jl")
 include("PsPotNL_UPF.jl")
 
+function create_pspots_UPF_HGH(atoms::Atoms; xcname="pz")
+    Nspecies = atoms.Nspecies
+    SpeciesSymbols = atoms.SpeciesSymbols
+    pspots = Array{PsPot_UPF}(undef,Nspecies)
+    for isp in 1:Nspecies
+        filename = SpeciesSymbols[isp]*"."*xcname*"-hgh.UPF"
+        pspots[isp] = PsPot_UPF(joinpath(UPF_HGH_DIR, filename))
+    end
+    return pspots
+end
+
 function test_main()
-    Random.seed!(1234)
-    filename = joinpath(DIR_STRUCTURES, "N2H4.xyz")
+    #filename = joinpath(DIR_STRUCTURES, "N2H4.xyz")
+    #filename = joinpath(DIR_STRUCTURES, "N2.xyz")
+    filename = joinpath(DIR_STRUCTURES, "Cl2.xyz")
     atoms = Atoms(ext_xyz_file=filename)
     pspots = Array{PsPot_UPF,1}(undef, atoms.Nspecies)
     #pspots[1] = PsPot_UPF("GBRV_LDA/n_lda_v1.2.uspp.F.UPF2")
     #pspots[2] = PsPot_UPF("GBRV_LDA/h_lda_v1.4.uspp.F.UPF2")
 
-    pspots[1] = PsPot_UPF("/home/efefer/pseudo/HGH/N.pz-hgh.UPF")
-    pspots[2] = PsPot_UPF("/home/efefer/pseudo/HGH/H.pz-hgh.UPF")
+    pspots = create_pspots_UPF_HGH(atoms)
 
-    print(pspots[1])
-    print(pspots[2])
-
+    for pspot in pspots
+        print(pspot)
+    end
     PsPotNL_UPF(atoms, pspots)
 end
 
