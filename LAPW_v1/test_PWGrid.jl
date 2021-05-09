dfb34cd3b3d8f430f924b339e52a8b30f478373d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-05-09 17:25:36 +0700

    Using dual keyword to PWGrid

--- LAPW_v1/test_PWGrid.jl
+++ LAPW_v1/test_PWGrid.jl
@@ -7,16 +7,20 @@ function main()
 
     gmaxvr = 12.0
     ecutrho = 0.5*gmaxvr^2
-    ecutwfc = ecutrho/4
-
-    pw = PWGrid(ecutwfc, LatVecs)
-    println(pw)
-
+    
     gkmax = 4.2452830189
     ecutwfc = 0.5*gkmax^2
-    kpoints = KPoints( 1, (1,1,1), zeros(3,1), [1.0], pw.RecVecs )
-    gvecw = PWDFT.init_gvecw( ecutwfc, pw.gvec, kpoints )
-    println(gvecw.Ngw)
+
+    dual = ecutrho/ecutwfc
+    println("dual = ", dual)
+
+    pw = PWGrid(ecutwfc, LatVecs, dual=dual)
+    println(pw)
+
+    #ecutwfc = 0.5*gkmax^2
+    #kpoints = KPoints( 1, (1,1,1), zeros(3,1), [1.0], pw.RecVecs )
+    #gvecw = PWDFT.init_gvecw( ecutwfc, pw.gvec, kpoints )
+    #println(gvecw.Ngw)
 end
 
 main()
