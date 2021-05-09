3fe8221d2dd0776ccb516a11fb3df3d7d01e2df0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-05-09 23:11:10 +0700

    Test sphcover with larger n

--- LAPW_v1/test_sphcover.jl
+++ LAPW_v1/test_sphcover.jl
@@ -3,11 +3,11 @@ using Printf
 include("sphcover.jl")
 
 function main()
-    n = 10
-    tp = zeros(2,10)
+    n = 100
+    tp = zeros(2,n)
     sphcover!(n, tp)
     for i in 1:n
-        @printf("%8d%18.10f%18.10f\n", i, tp[1,i], tp[2,i])
+        @printf(" %8d%18.10f%18.10f\n", i, tp[1,i], tp[2,i])
     end
 end
 
