8deaac19966ae74ee58b77b38fabf89be13e94b0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-05-10 15:39:30 +0700

    Using sphericalbesselj directly

--- LAPW_v1/test_sbessel.jl
+++ LAPW_v1/test_sbessel.jl
@@ -1,14 +1,14 @@
 using Printf
-using SpecialFunctions: besselj
+using SpecialFunctions: sphericalbesselj
 
-sbessel(l, x) = sqrt(pi/(2*x))*besselj(l+1/2, x)
+#sbessel(l, x) = sqrt(pi/(2*x))*besselj(l+1/2, x)
 
 function main()
     lmax = 4
     x = 1.2
     println("x = ", x)
     for l in 0:lmax
-        res = sbessel(l, x)
+        res = sphericalbesselj(l, x)
         @printf(" %4d%18.10f\n", l, res)
     end
 end
