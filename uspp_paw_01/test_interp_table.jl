000d1f2ec4fb9157860c1a7fa277d7d29b999387
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-07-10 10:17:22 +0700

    Test interpolation scheme for NLpsp proj

--- uspp_paw_01/test_interp_table.jl
+++ uspp_paw_01/test_interp_table.jl
@@ -51,12 +51,11 @@ function main()
                 qi = (iq - 1) * dq
                 for ir in 1:psp.kkbeta
                     jlqr = sphericalbesselj(l, qi*psp.r[ir])
-                    @printf("%8d %18.10f %18.10f %18.10f %18.10f\n", ir, qi, psp.r[ir],
-                        jlqr, 2*psp.proj_func[ir,ibeta])
+                    #@printf("%8d %18.10f %18.10f %18.10f %18.10f\n", ir, qi, psp.r[ir],
+                    #    jlqr, 2*psp.proj_func[ir,ibeta])
                     aux[ir] = psp.proj_func[ir,ibeta] * psp.r[ir] * jlqr
                 end
-                println("sum(aux) = ", 2*sum(aux[1:psp.kkbeta]))
-                exit()
+                #println("sum(aux) = ", 2*sum(aux[1:psp.kkbeta]))
                 vqint = integ_simpson( psp.kkbeta, aux, psp.rab )
                 interp_table[iq, ibeta, isp] = vqint * pref
             end
@@ -72,6 +71,23 @@ function main()
         @printf("%18.10f %18.10f\n", qi, interp_table[iq,ibeta,isp]*2)
     end
 
+    Gk2 = 2.0 # already multiplied by tpiba in QE
+    # Interpolation procedure
+    px = Gk2/dq - floor(Int64, Gk2/dq)
+    println("px = ", px)
+    ux = 1.0 - px
+    vx = 2.0 - px
+    wx = 3.0 - px
+    i0 = floor(Int64, Gk2/dq) + 1
+    println("i0 = ", i0)
+    i1 = i0 + 1
+    i2 = i0 + 2
+    i3 = i0 + 3
+    Vq = interp_table[i0,ibeta,isp] * ux * vx * wx / 6.0 +
+         interp_table[i1,ibeta,isp] * px * vx * wx / 2.0 -
+         interp_table[i2,ibeta,isp] * px * ux * wx / 2.0 +
+         interp_table[i3,ibeta,isp] * px * ux * vx / 6.0
+    @printf("Vq = %18.10f\n", Vq*2)
 end
 
 main()
