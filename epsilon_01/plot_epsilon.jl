e4eccabad980536b225df925ec0d67e39dd95dab
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-07-29 17:01:12 +0700

    Using Latex

--- epsilon_01/plot_epsilon.jl
+++ epsilon_01/plot_epsilon.jl
@@ -3,6 +3,9 @@ import Serialization
 import PyPlot
 const plt = PyPlot
 
+plt.rc("text", usetex=true)
+plt.rc("font", size=14)
+
 function main()
 
     wgrid = Serialization.deserialize("wgrid.data")    
@@ -11,13 +14,14 @@ function main()
 
     println("εr[1,1] = ", εr[1,1])
 
+    plt.figure(figsize=(7,8))
     plt.clf()
-    plt.plot(wgrid, εr[1,:], label="x-real")
-    plt.plot(wgrid, εi[1,:], label="x-imag")
+    plt.plot(wgrid, εr[1,:], label="\$\\epsilon_{1}\$")
+    plt.plot(wgrid, εi[1,:], label="\$\\epsilon_{2}\$")
     plt.grid(true)
     plt.xlim(0,6)
-    plt.xlabel("ħω (eV)")
-    plt.xlabel("ε")
+    plt.xlabel("\$\\hbar\\omega\$ (eV)")
+    plt.ylabel("\$\\epsilon\$")
     plt.legend()
     plt.savefig("IMG_epsilon.pdf")
 end
