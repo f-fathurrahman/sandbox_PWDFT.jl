75ab825c987358dfca7f812eab8b48b5a2286885
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-11-23 20:14:59 +0700

    Fix some problems reading PP_DIJ for full-relativistic ONCV pseudopotentials.

--- pspot_UPF_02/all_oncv.jl
+++ pspot_UPF_02/all_oncv.jl
@@ -1,28 +1,46 @@
 const FILELIST_ONCV_PBE = """
-Ag_ONCV_PBE-1.0.upf  Cl_ONCV_PBE-1.1.upf  H_ONCV_PBE-1.0.upf   Ne_ONCV_PBE-1.0.upf
-Sb_ONCV_PBE-1.1.upf  Te_ONCV_PBE-1.1.upf
-Al_ONCV_PBE-1.0.upf  C_ONCV_PBE-1.0.upf   In_ONCV_PBE-1.0.upf  Ni_ONCV_PBE-1.0.upf  Sc_ONCV_PBE-1.0.upf  
-Ti_ONCV_PBE-1.0.upf
-Ar_ONCV_PBE-1.0.upf  Co_ONCV_PBE-1.0.upf  In_ONCV_PBE-1.1.upf  N_ONCV_PBE-1.0.upf   Se_ONCV_PBE-1.0.upf  
-Tl_ONCV_PBE-1.0.upf
-Ar_ONCV_PBE-1.1.upf  Cr_ONCV_PBE-1.0.upf  I_ONCV_PBE-1.0.upf   O_ONCV_PBE-1.0.upf   Se_ONCV_PBE-1.1.upf  
-V_ONCV_PBE-1.0.upf
-As_ONCV_PBE-1.0.upf  Cs_ONCV_PBE-1.0.upf  I_ONCV_PBE-1.1.upf   Os_ONCV_PBE-1.0.upf  Si_ONCV_PBE-1.0.upf  
-W_ONCV_PBE-1.0.upf
-As_ONCV_PBE-1.1.upf  Cs_ONCV_PBE-1.1.upf  Ir_ONCV_PBE-1.0.upf  Pb_ONCV_PBE-1.0.upf  Si_ONCV_PBE-1.1.upf  
-W_ONCV_PBE-1.1.upf
-Au_ONCV_PBE-1.0.upf  Cu_ONCV_PBE-1.0.upf  K_ONCV_PBE-1.0.upf   Pd_ONCV_PBE-1.0.upf  Sn_ONCV_PBE-1.0.upf  
-Xe_ONCV_PBE-1.0.upf
-Ba_ONCV_PBE-1.0.upf  Fe_ONCV_PBE-1.0.upf  Kr_ONCV_PBE-1.0.upf  P_ONCV_PBE-1.0.upf   Sn_ONCV_PBE-1.1.upf  
-Xe_ONCV_PBE-1.1.upf
-Be_ONCV_PBE-1.0.upf  F_ONCV_PBE-1.0.upf   La_ONCV_PBE-1.0.upf  P_ONCV_PBE-1.1.upf   S_ONCV_PBE-1.0.upf   
-Y_ONCV_PBE-1.0.upf
-Bi_ONCV_PBE-1.0.upf  Ga_ONCV_PBE-1.0.upf  Li_ONCV_PBE-1.0.upf  Pt_ONCV_PBE-1.0.upf  S_ONCV_PBE-1.1.upf   
-Zn_ONCV_PBE-1.0.upf
-B_ONCV_PBE-1.0.upf   Ge_ONCV_PBE-1.0.upf  Mg_ONCV_PBE-1.0.upf  Rb_ONCV_PBE-1.0.upf  Sr_ONCV_PBE-1.0.upf  
-Zr_ONCV_PBE-1.0.upf
-Br_ONCV_PBE-1.0.upf  He_ONCV_PBE-1.0.upf  Mn_ONCV_PBE-1.0.upf  Re_ONCV_PBE-1.0.upf  Ta_ONCV_PBE-1.0.upf
-Ca_ONCV_PBE-1.0.upf  Hf_ONCV_PBE-1.0.upf  Mo_ONCV_PBE-1.0.upf  Rh_ONCV_PBE-1.0.upf  Ta_ONCV_PBE-1.1.upf
-Cd_ONCV_PBE-1.0.upf  Hf_ONCV_PBE-1.1.upf  Na_ONCV_PBE-1.0.upf  Ru_ONCV_PBE-1.0.upf  Tc_ONCV_PBE-1.0.upf
-Cl_ONCV_PBE-1.0.upf  Hg_ONCV_PBE-1.0.upf  Nb_ONCV_PBE-1.0.upf  Sb_ONCV_PBE-1.0.upf  Te_ONCV_PBE-1.0.upf
+Ag_ONCV_PBE-1.0.upf     Co_ONCV_PBE_FR-1.0.upf  Ir_ONCV_PBE-1.0.upf     Pb_ONCV_PBE-1.2.upf     S_ONCV_PBE-1.1.upf
+Ag_ONCV_PBE-1.2.upf     Cr_ONCV_PBE-1.0.upf     Ir_ONCV_PBE-1.2.upf     Pb_ONCV_PBE_FR-1.0.upf  S_ONCV_PBE-1.2.upf
+Ag_ONCV_PBE_FR-1.0.upf  Cr_ONCV_PBE-1.2.upf     Ir_ONCV_PBE_FR-1.0.upf  Pd_ONCV_PBE-1.0.upf     S_ONCV_PBE_FR-1.1.upf
+Al_ONCV_PBE-1.0.upf     Cr_ONCV_PBE_FR-1.0.upf  K_ONCV_PBE-1.0.upf      Pd_ONCV_PBE-1.2.upf     Sr_ONCV_PBE-1.0.upf
+Al_ONCV_PBE-1.2.upf     Cs_ONCV_PBE-1.0.upf     K_ONCV_PBE-1.2.upf      Pd_ONCV_PBE_FR-1.0.upf  Sr_ONCV_PBE-1.2.upf
+Al_ONCV_PBE_FR-1.0.upf  Cs_ONCV_PBE-1.1.upf     K_ONCV_PBE_FR-1.0.upf   P_ONCV_PBE-1.0.upf      Sr_ONCV_PBE_FR-1.0.upf
+Ar_ONCV_PBE-1.0.upf     Cs_ONCV_PBE-1.2.upf     Kr_ONCV_PBE-1.0.upf     P_ONCV_PBE-1.1.upf      Ta_ONCV_PBE-1.0.upf
+Ar_ONCV_PBE-1.1.upf     Cs_ONCV_PBE_FR-1.0.upf  Kr_ONCV_PBE-1.2.upf     P_ONCV_PBE-1.2.upf      Ta_ONCV_PBE-1.1.upf
+Ar_ONCV_PBE-1.2.upf     Cu_ONCV_PBE-1.0.upf     Kr_ONCV_PBE_FR-1.0.upf  P_ONCV_PBE_FR-1.1.upf   Ta_ONCV_PBE-1.2.upf
+Ar_ONCV_PBE_FR-1.1.upf  Cu_ONCV_PBE-1.2.upf     La_ONCV_PBE-1.0.upf     Pt_ONCV_PBE-1.0.upf     Ta_ONCV_PBE_FR-1.0.upf
+As_ONCV_PBE-1.0.upf     Cu_ONCV_PBE_FR-1.0.upf  La_ONCV_PBE-1.2.upf     Pt_ONCV_PBE-1.2.upf     Tc_ONCV_PBE-1.0.upf
+As_ONCV_PBE-1.1.upf     Fe_ONCV_PBE-1.0.upf     La_ONCV_PBE_FR-1.0.upf  Pt_ONCV_PBE_FR-1.0.upf  Tc_ONCV_PBE-1.2.upf
+As_ONCV_PBE-1.2.upf     Fe_ONCV_PBE-1.2.upf     Li_ONCV_PBE-1.0.upf     Rb_ONCV_PBE-1.0.upf     Tc_ONCV_PBE_FR-1.0.upf
+As_ONCV_PBE_FR-1.1.upf  Fe_ONCV_PBE_FR-1.0.upf  Li_ONCV_PBE-1.2.upf     Rb_ONCV_PBE-1.2.upf     Te_ONCV_PBE-1.0.upf
+Au_ONCV_PBE-1.0.upf     F_ONCV_PBE-1.0.upf      Mg_ONCV_PBE-1.0.upf     Rb_ONCV_PBE_FR-1.0.upf  Te_ONCV_PBE-1.1.upf
+Au_ONCV_PBE-1.2.upf     F_ONCV_PBE-1.2.upf      Mg_ONCV_PBE-1.2.upf     Re_ONCV_PBE-1.0.upf     Te_ONCV_PBE-1.2.upf
+Au_ONCV_PBE_FR-1.0.upf  F_ONCV_PBE_FR-1.0.upf   Mg_ONCV_PBE_FR-1.0.upf  Re_ONCV_PBE-1.2.upf     Te_ONCV_PBE_FR-1.1.upf
+Ba_ONCV_PBE-1.0.upf     Ga_ONCV_PBE-1.0.upf     Mn_ONCV_PBE-1.0.upf     Re_ONCV_PBE_FR-1.0.upf  Ti_ONCV_PBE-1.0.upf
+Ba_ONCV_PBE-1.2.upf     Ga_ONCV_PBE-1.2.upf     Mn_ONCV_PBE-1.2.upf     Rh_ONCV_PBE-1.0.upf     Ti_ONCV_PBE-1.2.upf
+Be_ONCV_PBE-1.0.upf     Ga_ONCV_PBE_FR-1.0.upf  Mn_ONCV_PBE_FR-1.0.upf  Rh_ONCV_PBE-1.2.upf     Ti_ONCV_PBE_FR-1.0.upf
+Be_ONCV_PBE-1.2.upf     Ge_ONCV_PBE-1.0.upf     Mo_ONCV_PBE-1.0.upf     Rh_ONCV_PBE_FR-1.0.upf  Tl_ONCV_PBE-1.0.upf
+Bi_ONCV_PBE-1.0.upf     Ge_ONCV_PBE-1.2.upf     Mo_ONCV_PBE-1.2.upf     Ru_ONCV_PBE-1.0.upf     Tl_ONCV_PBE-1.2.upf
+Bi_ONCV_PBE-1.2.upf     Ge_ONCV_PBE_FR-1.0.upf  Mo_ONCV_PBE_FR-1.0.upf  Ru_ONCV_PBE-1.2.upf     Tl_ONCV_PBE_FR-1.0.upf
+B_ONCV_PBE-1.0.upf      He_ONCV_PBE-1.0.upf     Na_ONCV_PBE-1.0.upf     Ru_ONCV_PBE_FR-1.0.upf  V_ONCV_PBE-1.0.upf
+B_ONCV_PBE-1.2.upf      He_ONCV_PBE-1.2.upf     Na_ONCV_PBE-1.2.upf     Sb_ONCV_PBE-1.0.upf     V_ONCV_PBE-1.2.upf
+B_ONCV_PBE_FR-1.0.upf   He_ONCV_PBE_FR-1.0.upf  Na_ONCV_PBE_FR-1.0.upf  Sb_ONCV_PBE-1.1.upf     V_ONCV_PBE_FR-1.0.upf
+Br_ONCV_PBE-1.0.upf     Hf_ONCV_PBE-1.0.upf     Nb_ONCV_PBE-1.0.upf     Sb_ONCV_PBE-1.2.upf     W_ONCV_PBE-1.0.upf
+Br_ONCV_PBE-1.2.upf     Hf_ONCV_PBE-1.1.upf     Nb_ONCV_PBE-1.2.upf     Sb_ONCV_PBE_FR-1.1.upf  W_ONCV_PBE-1.1.upf
+Br_ONCV_PBE_FR-1.0.upf  Hf_ONCV_PBE-1.2.upf     Nb_ONCV_PBE_FR-1.0.upf  Sc_ONCV_PBE-1.0.upf     W_ONCV_PBE-1.2.upf
+Ca_ONCV_PBE-1.0.upf     Hf_ONCV_PBE_FR-1.0.upf  Ne_ONCV_PBE-1.0.upf     Sc_ONCV_PBE-1.2.upf     W_ONCV_PBE_FR-1.0.upf
+Ca_ONCV_PBE-1.2.upf     Hg_ONCV_PBE-1.0.upf     Ne_ONCV_PBE-1.2.upf     Sc_ONCV_PBE_FR-1.0.upf  Xe_ONCV_PBE-1.0.upf
+Ca_ONCV_PBE_FR-1.0.upf  Hg_ONCV_PBE-1.2.upf     Ni_ONCV_PBE-1.0.upf     Se_ONCV_PBE-1.0.upf     Xe_ONCV_PBE-1.1.upf
+Cd_ONCV_PBE-1.0.upf     Hg_ONCV_PBE_FR-1.0.upf  Ni_ONCV_PBE-1.2.upf     Se_ONCV_PBE-1.1.upf     Xe_ONCV_PBE-1.2.upf
+Cd_ONCV_PBE-1.2.upf     H_ONCV_PBE-1.0.upf      Ni_ONCV_PBE_FR-1.0.upf  Se_ONCV_PBE-1.2.upf     Xe_ONCV_PBE_FR-1.1.upf
+Cd_ONCV_PBE_FR-1.0.upf  H_ONCV_PBE-1.2.upf      N_ONCV_PBE-1.0.upf      Se_ONCV_PBE_FR-1.1.upf  Y_ONCV_PBE-1.0.upf
+Cl_ONCV_PBE-1.0.upf     H_ONCV_PBE_FR-1.0.upf   N_ONCV_PBE-1.2.upf      Si_ONCV_PBE-1.0.upf     Y_ONCV_PBE-1.2.upf
+Cl_ONCV_PBE-1.1.upf     In_ONCV_PBE-1.0.upf     N_ONCV_PBE_FR-1.0.upf   Si_ONCV_PBE-1.1.upf     Y_ONCV_PBE_FR-1.0.upf
+Cl_ONCV_PBE-1.2.upf     In_ONCV_PBE-1.1.upf     O_ONCV_PBE-1.0.upf      Si_ONCV_PBE-1.2.upf     Zn_ONCV_PBE-1.0.upf
+Cl_ONCV_PBE_FR-1.1.upf  In_ONCV_PBE-1.2.upf     O_ONCV_PBE-1.2.upf      Si_ONCV_PBE_FR-1.1.upf  Zn_ONCV_PBE-1.2.upf
+C_ONCV_PBE-1.0.upf      In_ONCV_PBE_FR-1.0.upf  O_ONCV_PBE_FR-1.0.upf   Sn_ONCV_PBE-1.0.upf     Zn_ONCV_PBE_FR-1.0.upf
+C_ONCV_PBE-1.2.upf      I_ONCV_PBE-1.0.upf      Os_ONCV_PBE-1.0.upf     Sn_ONCV_PBE-1.1.upf     Zr_ONCV_PBE-1.0.upf
+C_ONCV_PBE_FR-1.0.upf   I_ONCV_PBE-1.1.upf      Os_ONCV_PBE-1.2.upf     Sn_ONCV_PBE-1.2.upf     Zr_ONCV_PBE-1.2.upf
+Co_ONCV_PBE-1.0.upf     I_ONCV_PBE-1.2.upf      Os_ONCV_PBE_FR-1.0.upf  Sn_ONCV_PBE_FR-1.1.upf  Zr_ONCV_PBE_FR-1.0.upf
+Co_ONCV_PBE-1.2.upf     I_ONCV_PBE_FR-1.1.upf   Pb_ONCV_PBE-1.0.upf     S_ONCV_PBE-1.0.upf
 """
--- pspot_UPF_02/test_ONCV.jl
+++ pspot_UPF_02/test_ONCV.jl
@@ -1,13 +1,13 @@
 using Printf
 import LightXML
+using PWDFT: PsPot_UPF
 
-include("PsPot_UPF.jl")
 include("all_oncv.jl")
 
 function test_ONCV_PBE()
     list_file = split(FILELIST_ONCV_PBE, keepempty=false)
     for f in list_file
-        psp = PsPot_UPF(joinpath("/home/efefer/pseudo/ONCV_PBE", f))
+        psp = PsPot_UPF(joinpath("/home/efefer/pseudo/ONCV_PBE_v1.2", f))
         println("\nf = ", f)
         println("Nproj = ", psp.Nproj)
         println("proj_l = ", psp.proj_l)
