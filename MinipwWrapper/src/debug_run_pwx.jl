2be088249dace4cd61bea543bde75cfd4989abb5
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-07-09 21:21:06 +0700

    Add call to info_upf, check is_multiproj

--- MinipwWrapper/src/debug_run_pwx.jl
+++ MinipwWrapper/src/debug_run_pwx.jl
@@ -1,4 +1,5 @@
 const LIBMINIPW = "/home/efefer/WORKS/my_github_repos/ffr-learns-qe/minipw-6.6/srclib/libqemain.so"
 
 ccall( (:prepare_all_, LIBMINIPW), Cvoid, () )
+ccall( (:info_upf_, LIBMINIPW), Cvoid, () )
 ccall( (:my_electrons_, LIBMINIPW), Cvoid, () )
