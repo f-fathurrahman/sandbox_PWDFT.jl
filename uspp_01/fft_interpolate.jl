c5c33dbe6d9913005bfea9d145b3a66051f5242b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-07-16 22:58:23 +0700

    Dense to smooth interp

---
+++ uspp_01/fft_interpolate.jl
@@ -0,0 +1,67 @@
+function dense_to_smooth!(
+    pw::PWGrid, v_in, v_out
+)
+
+    # Immediate return if not using dual grid
+    if !pw.using_dual_grid
+        v_out[:] = v_in[:]
+        return
+    end
+
+    NptsDense = prod(pw.Ns)
+    NptsSmooth = prod(pw.Nss) # not used?
+
+    aux_in = zeros(ComplexF64, NptsDense)
+
+    # Copy all data to aux_in
+    aux_in[1:NptsDense] .= v_in[1:NptsDense]
+
+    R_to_G!(pw, aux_in) # need a way to tell which plan to use
+
+    println("Some aux_in after R_to_G! (scaled)")
+    for i in 1:10
+        @printf("%5d %18.10f %18.10f\n", i, aux_in[i].re/NptsDense, aux_in[i].im/NptsDense)
+    end
+
+    println("sum aux_in, scaled = ", sum(aux_in)/NptsDense)
+
+    # Zero out aux_out (not needed)
+
+    aux_out = zeros(ComplexF64, NptsSmooth)
+    Ngs = pw.gvecs.Ng
+    println("Ngs = ", Ngs)
+    for ig in 1:Ngs
+        ip_out = pw.gvecs.idx_g2r[ig] # smooth
+        ip_in  = pw.gvec.idx_g2r[ig] # dense
+        aux_out[ip_out] = aux_in[ip_in]
+    end
+
+    println("Compare ip_in, ip_out")
+    for ig in 1:10
+        ip_out = pw.gvecs.idx_g2r[ig]
+        ip_in  = pw.gvec.idx_g2r[ig]
+        @printf("%5d %5d\n", ip_in, ip_out)
+    end
+
+    println("Some aux_out before G_to_R! (scaled)")
+    for i in NptsSmooth-10:NptsSmooth
+        @printf("%8d %18.10f %18.10f\n", i, aux_out[i].re/NptsDense, aux_out[i].im/NptsDense)
+    end
+
+    println("Before G_to_R!: sum(aux_out), scaled = ", sum(aux_out)/NptsDense)
+    
+    G_to_R!(pw, aux_out, smooth=true)
+    # FIXME: Manual normalization
+    aux_out *= (NptsSmooth/NptsDense)  # XXX This is important!
+
+    println("Some aux_out after G_to_R!")
+    for i in 1:10
+        @printf("%5d %18.10f %18.10f\n", i, aux_out[i].re, aux_out[i].im)
+    end
+
+    println("After G_to_R!: sum(aux_out) = ", sum(aux_out))
+
+    v_out[1:NptsSmooth] = real(aux_out[1:NptsSmooth])
+
+    return
+end
---
+++ uspp_01/test_smooth_to_dense.jl
@@ -0,0 +1,58 @@
+using Printf
+using OffsetArrays
+using SpecialFunctions: sphericalbesselj
+
+using PWDFT
+
+include("../pwscf_02/PWSCFInput.jl")
+
+function init_Ham_from_pwinput()
+    println("ARGS = ", ARGS)
+    @assert length(ARGS) == 1
+    pwinput = PWSCFInput(ARGS[1])
+
+    atoms = pwinput.atoms
+
+    ecutwfc = pwinput.ecutwfc
+    ecutrho = pwinput.ecutrho
+    dual = ecutrho/ecutwfc
+
+    pspfiles = pwinput.pspfiles
+    # Need special treatement for GTH ?
+
+    return Hamiltonian(atoms, pspfiles, ecutwfc, dual=dual)
+end
+
+include("fft_interpolate.jl")
+
+function test_main()
+    
+    Ham = init_Ham_from_pwinput()
+
+    @assert Ham.pw.using_dual_grid
+
+    NptsDense = prod(Ham.pw.Ns)
+    NptsSmooth = prod(Ham.pw.Nss)
+
+    vin = zeros(Float64, NptsDense)
+    vout = zeros(Float64, NptsSmooth)
+
+    vin[:] .= 1.1
+    vin[1:10] .= 2.5
+
+    println("sum vin before dense_to_smooth = ", sum(vin))
+    println("sum vout before dense_to_smooth = ", sum(vout))
+
+    dense_to_smooth!( Ham.pw, vin, vout )
+
+    println("sum vin after dense_to_smooth = ", sum(vin))
+    println("sum vout after dense_to_smooth = ", sum(vout))
+
+    println("Some vin and vout")
+    for i in 1:10
+        @printf("%5d %18.10f %18.10f\n", i, vin[i], vout[i])
+    end
+
+end
+
+test_main()
