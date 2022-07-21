42a2e5b032cc87de4769042a29e3165713f55891
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-07-21 20:24:15 +0700

    newd running, need checking

--- uspp_01/newd.jl
+++ uspp_01/newd.jl
@@ -2,13 +2,14 @@ function calc_QVeff!( Ham )
 
     Nspin = Ham.electrons.Nspin
     Ng = Ham.pw.gvec.Ng
-    lmaxq = Ham.pspotNL.lmaxq
+    lmaxkb = Ham.pspotNL.lmaxkb
     G2 = Ham.pw.gvec.G2
     G = Ham.pw.gvec.G
     Npoints = prod(Ham.pw.Ns)
-    idx_g2r = Ham.pw.idx_g2r
+    idx_g2r = Ham.pw.gvec.idx_g2r
     Veff = Ham.potentials.Total
 
+    lmaxq = 2*lmaxkb + 1 # using 1-indexing
     ylmk0 = zeros(Float64, Ng, lmaxq*lmaxq)
     _lmax = lmaxq - 1 # or 2*lmaxkb
     # Ylm_real_qe accept l value starting from 0
@@ -23,7 +24,7 @@ function calc_QVeff!( Ham )
             ctmp[ip] = Veff[ip,ispin] # Veff already contains Ps_loc
         end
         R_to_G!(Ham.pw, ctmp)
-        end ig in 1:Ng
+        for ig in 1:Ng
             ip = idx_g2r[ig]
             VeffG[ig,ispin] = ctmp[ip]
         end
@@ -33,6 +34,7 @@ function calc_QVeff!( Ham )
     Nspecies = Ham.atoms.Nspecies
     Natoms = Ham.atoms.Natoms
     atm2species = Ham.atoms.atm2species
+    atpos = Ham.atoms.positions
     nh = Ham.pspotNL.nh
     Deeq = Ham.pspotNL.Deeq
     CellVolume = Ham.pw.CellVolume
@@ -41,9 +43,10 @@ function calc_QVeff!( Ham )
 
         if Ham.pspots[isp].is_ultrasoft
             # nij = max number of (ih,jh) pairs per atom type nt
-            nij = nh[isp] * (nh[isp] + 1)/2
+            nij = round(Int64, nh[isp] * (nh[isp] + 1)/2 )
 
             Qgm = zeros(ComplexF64, Ng, nij)
+            println("nij = ", nij)
             #
             # Compute and store Q(G) for this atomic species 
             # (without structure factor)
@@ -54,15 +57,15 @@ function calc_QVeff!( Ham )
                 @views qvan2!( Ham.pspotNL, ih, jh, isp, G2, ylmk0, Qgm[:,ijh] )
             end
             #
-            # count max number of atoms of type nt
-            #
-            nab = 0
-            for ia in 1:Natoms
-                if atm2species[ia] != isp
-                    continue
-                end
-                nab = nab + 1
-            end
+            # count max number of atoms of type isp
+            nab = sum(atm2species .== isp)
+            # nab = 0
+            #for ia in 1:Natoms
+            #    if atm2species[ia] != isp
+            #        continue
+            #    end
+            #    nab = nab + 1
+            #end
             #
             aux = zeros(ComplexF64, Ng, nab)
             #
@@ -80,30 +83,36 @@ function calc_QVeff!( Ham )
                         aux[ig,nb] = VeffG[ig,ispin] * Sf
                     end
                 end
-            #
-            # here we compute the integral Q*V for all atoms of this kind
-            deeaux = Qgm*aux
-            #CALL DGEMM( 'C', 'N', nij, nab, 2*ngm, fact, qgm, 2*ngm, aux, &
-            #         2*ngm, 0.0_dp, deeaux, nij )
+                #
+                # here we compute the integral Q*V for all atoms of this kind
+                deeaux = real(Qgm' * aux)
+                #CALL DGEMM( 'C', 'N', nij, nab, 2*ngm, fact, qgm, 2*ngm, aux, &
+                #         2*ngm, 0.0_dp, deeaux, nij )
+
+                println("isp = ", isp, " size deeaux = ", size(deeaux))
         
-            nb = 0
-            for ia in 1:Natoms
-                if atm2species[ia] != isp
-                    continue
-                end
-                nb = nb + 1
-                ijh = 0
-                for ih in 1:nh[isp], jh = ih:nh[isp]
-                    ijh = ijh + 1
-                    Deeq[ih,jh,ia,ispin] = CellVolume * deeaux[ijh,nb]
-                    if jh > ih
-                        Deeq[jh,ih,ia,ispin] = Deeq[ih,jh,ia,ispin]
+                nb = 0
+                for ia in 1:Natoms
+                    if atm2species[ia] != isp
+                        continue
                     end
-                end
-            end # Natoms
+                    nb = nb + 1
+                    ijh = 0
+                    for ih in 1:nh[isp], jh in ih:nh[isp]
+                        ijh = ijh + 1
+                        Deeq[ih,jh,ia,ispin] = CellVolume * deeaux[ijh,nb]
+                        if jh > ih
+                            Deeq[jh,ih,ia,ispin] = Deeq[ih,jh,ia,ispin]
+                        end
+                    end
+                end # Natoms
+
+            end # Nspin
         
         end # is_ultrasoft
     
     end # Nspecies
+
+    return
 
 end
