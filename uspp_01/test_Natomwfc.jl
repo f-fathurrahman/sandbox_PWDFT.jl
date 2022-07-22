4141fb8513aca9a1ee408df1609d79263e703e35
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-07-22 09:57:07 +0700

    Test calc_Natomwfc, working only for PsPot_UPF

---
+++ uspp_01/test_Natomwfc.jl
@@ -0,0 +1,39 @@
+using Printf
+using OffsetArrays
+using SpecialFunctions: sphericalbesselj
+
+using PWDFT
+
+include("../pwscf_02/PWSCFInput.jl")
+include("atomic_rho_g.jl")
+include("dense_to_smooth.jl")
+include("newd.jl")
+
+function init_Ham_from_pwinput()
+    println("ARGS = ", ARGS)
+    @assert length(ARGS) == 1
+    pwinput = PWSCFInput(ARGS[1])
+
+    atoms = pwinput.atoms
+
+    ecutwfc = pwinput.ecutwfc
+    ecutrho = pwinput.ecutrho
+    dual = ecutrho/ecutwfc
+
+    pspfiles = pwinput.pspfiles
+    # Need special treatement for GTH ?
+
+    return Hamiltonian(atoms, pspfiles, ecutwfc, dual=dual)
+end
+
+
+
+function test_main()
+    
+    Ham = init_Ham_from_pwinput()
+    Natomwfc = calc_Natomwfc(Ham.atoms, Ham.pspots)
+    println("Natomwfc = ", Natomwfc)
+
+end
+
+test_main()
