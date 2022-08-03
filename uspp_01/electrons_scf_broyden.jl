b4d13143e853f63c1c10705f01010d30e30de4ca
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-08-03 23:32:25 +0700

    Implement and test electrons_scf_broyden

---
+++ uspp_01/electrons_scf_broyden.jl
@@ -0,0 +1,134 @@
+function electrons_scf_broyden!(
+    Ham::Hamiltonian, psiks;
+    NiterMax=150,
+    betamix=0.2,
+    etot_conv_thr=1e-6,
+    ethr_evals_last=1e-13
+)
+
+    Ham.energies.NN = calc_E_NN(Ham.atoms)
+
+    Etot_old = 0.0
+    Nconv = 0
+
+    Npoints = prod(Ham.pw.Ns)
+    CellVolume = Ham.pw.CellVolume
+    dVol = CellVolume/Npoints
+    Nstates = Ham.electrons.Nstates
+
+    Rhoe = Ham.rhoe
+    println("Initial integ Rhoe = ", sum(Rhoe)*dVol)
+
+    diffRhoe = 0.0
+
+    @printf("\n")
+    @printf("SCF iteration starts (with density mixing), betamix = %f\n", betamix)
+    @printf("\n")
+
+    Vin = zeros(Float64, Npoints)
+    Nspin = Ham.electrons.Nspin
+    Nkpt = Ham.pw.gvecw.kpoints.Nkpt
+
+    Vhartree = Ham.potentials.Hartree
+    Vxc = Ham.potentials.XC
+    Focc = Ham.electrons.Focc
+    wk = Ham.pw.gvecw.kpoints.wk
+
+    evals = zeros(Float64, Nstates, Nkpt*Nspin)
+    
+    mixdim = 8
+    df = zeros(Float64,Npoints*Nspin, mixdim)
+    dv = zeros(Float64,Npoints*Nspin, mixdim)
+
+    ethr = 1e-5 # default
+
+    for iterSCF in 1:NiterMax
+        
+        # determine convergence criteria for diagonalization
+        if iterSCF == 1
+            ethr = 0.1
+        elseif iterSCF == 2
+            ethr = 0.01
+        else
+            ethr = ethr/5.0
+            ethr = max( ethr, ethr_evals_last )
+        end
+
+        println("\niterSCF = ", iterSCF)
+        println("Davidson diagonalization with ethr = ", ethr)
+        evals[:,:] = diag_davidson_qe!( Ham, psiks, tol=ethr )
+
+        println("Eigenvalues in eV: ikspin = 1")
+        for ist in 1:Nstates
+            @printf("%5d %18.10f\n", ist, evals[ist,1]*Ha2eV)
+        end
+
+        Eband = 0.0
+        for ispin in 1:Nspin, ik in 1:Nkpt
+            ikspin = ik + (ispin - 1)*Nkpt
+            for ist in 1:Nstates
+                Eband = Eband + wk[ik]*Focc[ist,ikspin]*evals[ist,ikspin]
+            end
+        end
+
+        Rhoe_new = calc_rhoe_uspp( Ham, psiks )
+        println("integ Rhoe_new = ", sum(Rhoe_new)*dVol)
+
+        # 
+        Vin[:] = Vhartree[:] + Vxc[:,1]
+        deband = -sum(Vin .* Rhoe_new[:,1])*dVol
+
+        #
+        # Mix the density
+        #
+        mix_broyden!( Rhoe, Rhoe_new, betamix, iterSCF, mixdim, df, dv )
+
+        println("integ Rhoe after mix: ", sum(Rhoe)*dVol)
+
+        diffRhoe = norm(Rhoe - Rhoe_new)
+
+        #
+        Ehartree, Exc, Evtxc = update_from_rhoe!(Ham, Rhoe)
+
+        descf = sum( (Rhoe[:,1] .- Rhoe_new[:,1]).*(Vhartree + Vxc[:,1]) )*dVol
+
+        Etot = Eband + deband + Ehartree + Exc + Ham.energies.NN + descf # entropy missed
+        
+        println("-----------------------")        
+        println("Energy components in Ry")
+        println("-----------------------")
+        @printf("Eband    = %18.10f\n", Eband*2)
+        @printf("deband   = %18.10f\n", deband*2)
+        @printf("descf    = %18.10f\n", descf*2)
+        @printf("-----------------------------\n")
+        @printf("OneEle   = %18.10f\n", 2*(Eband + deband))
+        @printf("Ehartree = %18.10f\n", 2*Ehartree)
+        @printf("Exc      = %18.10f\n", 2*Exc)
+        @printf("NN       = %18.10f\n", 2*Ham.energies.NN)
+        @printf("-----------------------------\n")
+        @printf("Total    = %18.10f\n", 2*Etot)
+
+        #
+        diffEtot = abs(Etot - Etot_old)
+        @printf("\nSCF: %5d %18.10f %10.5e %10.5e\n", iterSCF, Etot, diffEtot, diffRhoe)
+        if diffEtot <= etot_conv_thr
+            Nconv = Nconv + 1
+        else
+            Nconv = 0
+        end
+        if Nconv >= 2
+            @printf("SCF is converged in %d iterations\n", iterSCF)
+            return
+        end
+
+        #if diffRhoe < 1e-5
+        #    @printf("converged by diffRhoe\n")
+        #    return
+        #end
+
+        Etot_old = Etot
+        flush(stdout)
+    end
+    @printf("WARNING: SCF is not converged after %d iterations\n", NiterMax)
+    return
+end
---
+++ uspp_01/test_electrons_scf_broyden.jl
@@ -0,0 +1,85 @@
+using Printf
+using OffsetArrays
+using LinearAlgebra
+using SpecialFunctions: sphericalbesselj
+
+using Random
+Random.seed!(1234)
+
+using PWDFT
+
+include("../pwscf_02/PWSCFInput.jl")
+include("../pwscf_02/init_Ham_from_pwinput.jl")
+
+include("atomic_rho_g.jl")
+include("dense_to_smooth.jl")
+include("smooth_to_dense.jl")
+include("update_from_rhoe.jl")
+include("newd.jl")
+include("op_S.jl")
+include("calc_rhoe_uspp.jl")
+include("../diag_davidson_qe/diag_davidson_qe_v2.jl")
+
+include("electrons_scf_broyden.jl")
+
+
+function ortho_sqrt_with_S!( Ham::Hamiltonian, psi::Array{ComplexF64,2} )
+    O = psi' * op_S(Ham, psi)
+    Udagger = inv(sqrt(O))
+    psi[:,:] = psi*Udagger
+    return
+end
+
+
+function ortho_check_with_S( Ham::Hamiltonian, psi::Array{ComplexF64,2}; dVol=1.0 )
+    Nstates = size(psi,2)
+    Spsi = op_S(Ham, psi)
+    @printf("\nNorm check:\n")
+    for ist = 1:Nstates
+        c = dot( psi[:,ist], Spsi[:,ist] ) * dVol
+        @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
+    end
+    @printf("\nOrtho check w.r.t state #1:\n")
+    for ist = 1:Nstates
+        c = dot( psi[:,ist], Spsi[:,2] ) * dVol
+        @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
+    end
+    @printf("\n")
+    return
+end
+
+
+
+function prepare_scf!(Ham)
+    # Initial density
+    Rhoe, RhoeG = atomic_rho_g(Ham)
+    # Update the potentials
+    _ = update_from_rhoe!( Ham, Rhoe, RhoeG )
+    # Initial wavefunc
+    psiks = rand_BlochWavefunc(Ham)
+    Nkpt = Ham.pw.gvecw.kpoints.Nkpt
+    Nspin = Ham.electrons.Nspin
+    # Reorthonormalize (FIXME: need to be included in rand_Blochwavefunc)
+    for ispin in 1:Nspin, ik in 1:Nkpt
+        Ham.ispin = ispin
+        Ham.ik = ik
+        ikspin = ik + (ispin - 1)*Nkpt
+        ortho_sqrt_with_S!(Ham, psiks[ikspin])
+    end
+    return psiks
+end
+
+
+
+function test_main()
+
+    Ham = init_Ham_from_pwinput()
+    println(Ham)
+
+    psiks = prepare_scf!(Ham)
+
+    electrons_scf_broyden!(Ham, psiks, NiterMax=100)
+
+end
+
+test_main()
