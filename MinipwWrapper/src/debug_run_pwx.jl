6d637b2b81ef17512b681d72ef7ba2bc2919c0da
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-08-19 18:54:49 +0700

    Also call my_forces

--- MinipwWrapper/src/debug_run_pwx.jl
+++ MinipwWrapper/src/debug_run_pwx.jl
@@ -3,3 +3,4 @@ const LIBMINIPW = "/home/efefer/WORKS/my_github_repos/ffr-learns-qe/minipw-6.6/s
 ccall( (:prepare_all_, LIBMINIPW), Cvoid, () )
 ccall( (:info_upf_, LIBMINIPW), Cvoid, () )
 ccall( (:my_electrons_, LIBMINIPW), Cvoid, () )
+ccall( (:my_forces_, LIBMINIPW), Cvoid, () )
