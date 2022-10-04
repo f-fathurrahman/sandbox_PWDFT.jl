98b544e5c1a1c8b387d1c37d9c2fb24d31b51c3f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-10-04 20:27:31 +0700

    electron_scf_broyden_v2, try to imitate pw.x scf scheme

--- uspp_01/test_electrons_scf_broyden.jl
+++ uspp_01/test_electrons_scf_broyden.jl
@@ -20,67 +20,18 @@ include("op_S.jl")
 include("calc_rhoe_uspp.jl")
 include("../diag_davidson_qe/diag_davidson_qe_v2.jl")
 
-include("electrons_scf_broyden.jl")
+#include("electrons_scf_broyden.jl")
+include("electrons_scf_broyden_v2.jl")
 include("mix_broyden_02.jl")
 #include("mix_broyden_03.jl")
 
-function ortho_sqrt_with_S!( Ham::Hamiltonian, psi::Array{ComplexF64,2} )
-    O = psi' * op_S(Ham, psi)
-    Udagger = inv(sqrt(O))
-    psi[:,:] = psi*Udagger
-    return
-end
-
-
-function ortho_check_with_S( Ham::Hamiltonian, psi::Array{ComplexF64,2}; dVol=1.0 )
-    Nstates = size(psi,2)
-    Spsi = op_S(Ham, psi)
-    @printf("\nNorm check:\n")
-    for ist = 1:Nstates
-        c = dot( psi[:,ist], Spsi[:,ist] ) * dVol
-        @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
-    end
-    @printf("\nOrtho check w.r.t state #1:\n")
-    for ist = 1:Nstates
-        c = dot( psi[:,ist], Spsi[:,2] ) * dVol
-        @printf("State: #%5d: (%18.10f,%18.10f)\n", ist, c.re, c.im)
-    end
-    @printf("\n")
-    return
-end
-
-
-
-function prepare_scf!(Ham)
-    # Initial density
-    Rhoe, RhoeG = atomic_rho_g(Ham)
-    # Update the potentials
-    _ = update_from_rhoe!( Ham, Rhoe, RhoeG )
-    # Initial wavefunc
-    psiks = rand_BlochWavefunc(Ham)
-    Nkpt = Ham.pw.gvecw.kpoints.Nkpt
-    Nspin = Ham.electrons.Nspin
-    # Reorthonormalize (FIXME: need to be included in rand_Blochwavefunc)
-    for ispin in 1:Nspin, ik in 1:Nkpt
-        Ham.ispin = ispin
-        Ham.ik = ik
-        ikspin = ik + (ispin - 1)*Nkpt
-        ortho_sqrt_with_S!(Ham, psiks[ikspin])
-    end
-    return psiks
-end
-
-
+include("ortho_with_S.jl")
 
 function test_main()
-
     Ham = init_Ham_from_pwinput()
     println(Ham)
-
-    psiks = prepare_scf!(Ham)
-
+    psiks = rand_BlochWavefunc(Ham)
     electrons_scf_broyden!(Ham, psiks, NiterMax=100)
-
 end
 
 test_main()
