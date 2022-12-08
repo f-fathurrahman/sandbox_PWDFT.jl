918c8247a4673081dbaecf7aa42ee8f9356242b6
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-12-08 17:55:45 +0700

    Test metaGGA for Si fcc

---
+++ mgga_01/main_Si_fcc.jl
@@ -0,0 +1,44 @@
+using LinearAlgebra
+using Random
+using Printf
+
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+import InteractiveUtils
+InteractiveUtils.versioninfo()
+import Dates
+println("Now = ", Dates.now())
+
+function main( ; method="SCF" )
+
+    Random.seed!(1234)
+
+    # Atoms
+    atoms = Atoms(xyz_string_frac=
+        """
+        2
+
+        Si  0.0  0.0  0.0
+        Si  0.25  0.25  0.25
+        """, in_bohr=true, LatVecs=gen_lattice_fcc(10.2631))
+
+    # Initialize Hamiltonian
+    pspfiles = [joinpath(DIR_PSP, "Si-q4.gth")]
+    ecutwfc = 15.0
+    Ham = Hamiltonian( atoms, pspfiles, ecutwfc, meshk=[3,3,3], Ns_=(32,32,32), xcfunc="SCAN" )
+    println(Ham)
+
+    #
+    # Solve the KS problem
+    #
+
+    KS_solve_Emin_PCG!( Ham, verbose=true )
+    #KS_solve_SCF!( Ham )
+end
+
+@time main(method="Emin")
+#@time main(method="Emin")
