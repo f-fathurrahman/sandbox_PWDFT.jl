9ae85003cc4c9d0f1833c07f82e8ccfd0f917416
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-01-30 07:41:52 +0700

    Test read PAW again

--- paw_01/test_read_paw_upf.jl
+++ paw_01/test_read_paw_upf.jl
@@ -29,16 +29,16 @@ function test_main()
         filepath = joinpath(prefix_dir, f)
         println("\nfilepath = ", filepath)
         #
-        #test_paw(filepath)
-        #
-        psp = PsPot_UPF(joinpath(prefix_dir, f))
-        #
-        println("psp.lmax = ", psp.lmax)
-        println("psp.lmax_rho = ", psp.lmax_rho)
-        println("psp.nqf = ", psp.nqf)
-        println("psp.nqlc = ", psp.nqlc)
-        println("psp.q_with_l = ", psp.q_with_l)
-        println("psp.is_paw = ", psp.is_paw)
+        test_paw(filepath)
+        # Also check PsPot_UPF
+        #psp = PsPot_UPF(joinpath(prefix_dir, f))
+        ##
+        #println("psp.lmax = ", psp.lmax)
+        #println("psp.lmax_rho = ", psp.lmax_rho)
+        #println("psp.nqf = ", psp.nqf)
+        #println("psp.nqlc = ", psp.nqlc)
+        #println("psp.q_with_l = ", psp.q_with_l)
+        #println("psp.is_paw = ", psp.is_paw)
     end
 end
 
