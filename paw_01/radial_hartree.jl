7efe52f41e7dfa18b6af05f1f87b8997cb9076bf
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-04-16 23:24:27 +0700

    Draft PAW_h_potential

--- paw_01/radial_hartree.jl
+++ paw_01/radial_hartree.jl
@@ -1,10 +1,17 @@
 #
 # Solution of the Poisson's equation on a radial (logarithmic) grid
 #---------------------------------------------------------------
-function radial_hartree!( k, nst, mesh, grid, f, vh )
+function radial_hartree!(
+    k, nst,
+    r::Vector{Float64}, dx,
+    f, vh
+)
 
-    # use upf_kinds, only : DP
-    # implicit none
+    Nrmesh = size(r, 1)
+    # FIXME: We recalculate some quantities here
+    r2 = r.^2
+    sqr = sqrt.(r)
+
     # integer,intent(in)::       & 
     #      k,   & ! input: the k of the equation
     #      nst, & ! input: at low r, f goes as r**nst
@@ -45,10 +52,9 @@ function radial_hartree!( k, nst, mesh, grid, f, vh )
     #! Allocate space for the diagonal and off diagonal elements
     #!
     #if (mesh.ne.grid%mesh) call upf_error('hartree',' grid dimension mismatch',1) 
-    #allocate(d(mesh),stat=ierr)
-    #allocate(e(mesh),stat=ierr)
-
-    # if( ierr .ne. 0) call upf_error('hartree',' error allocating d or e',1)
+    
+    d = zeros(Float64, Nrmesh)
+    e = zeros(Float64, Nrmesh)
     
     #
     # Find the series expansion of the solution close to r=0
@@ -57,18 +63,16 @@ function radial_hartree!( k, nst, mesh, grid, f, vh )
     nk1 = nst - k - 1
     if nk1 <= 0
         error("nk1 is less than or equal to zero")
-        #!else if(nk1.ge.4) then
-        #! not sure whether the following is really correct, but the above wasn't
     elseif nk1 >= 3
         c2 = 0.0
         c3 = 0.0
     else
-        e[1] = 0.0_dp
+        e[1] = 0.0
         for i in 1:4
-           d[i] = -k21*f(i)/grid.r[i]^nst
+           d[i] = -k21*f[i]/r[i]^nst
         end
         # use four points nk1:nk1+3
-        @views radial_grid_series!( d, grid.r, grid.r2, e[nk1:nk1+3] )
+        @views radial_grid_series!( d, r, r2, e[nk1:nk1+3] )
         c2 = e[1]/(4.0*k + 6.0)
         c3 = e[2]/(6.0*k + 12.0)
     end
@@ -76,8 +80,8 @@ function radial_hartree!( k, nst, mesh, grid, f, vh )
     #
     # Set the main auxiliary parameters
     #
-    ch = grid.dx * grid.dx / 12.0
-    xkh2 = ch*( k + 0.5)^2
+    ch = dx^2 / 12.0
+    xkh2 = ch*(k + 0.5)^2
     ei = 1.0 - xkh2
     di = -(2.0 + 10.0*xkh2)
 
@@ -88,7 +92,7 @@ function radial_hartree!( k, nst, mesh, grid, f, vh )
     for i in 2:Nrmesh
        d[i] = -di
        e[i] = -ei
-       vh[i] = k21*ch*grid.sqr[i]*f[i]
+       vh[i] = k21*ch*sqr[i]*f[i]
     end
   
     #
@@ -96,20 +100,20 @@ function radial_hartree!( k, nst, mesh, grid, f, vh )
     # solution in the first point from the first equation. This 
     # part for the diagonal element
     #
-    f1 = (grid.sqr[1]/grid.sqr[2])^k21
+    f1 = (sqr[1]/sqr[2])^k21
     d[2] = d[2] - ei*f1
     
     #
     # Use the boundary condition to eliminate the value of the 
     # solution in the last point from the last equation
     #
-    fn = (grid.sqr[Nrmesh-1]/grid%sqr[Nrmesh])^k21
-    d[mesh-1] = d[Nrmesh-1] - ei*fn
+    fn = (sqr[Nrmesh-1]/sqr[Nrmesh])^k21
+    d[Nrmesh-1] = d[Nrmesh-1] - ei*fn
   
     #
     # In the first point vh(1) has the same definition as in the other points
     #
-    vhim1 = k21*ch*grid.sqr[1]*f[1]
+    vhim1 = k21*ch*sqr[1]*f[1]
   
     #
     # Compute the right hand side using the auxiliary quantity vh(i).
@@ -124,31 +128,30 @@ function radial_hartree!( k, nst, mesh, grid, f, vh )
     # Use the boundary condition to eliminate the value of the solution in the 
     # first point from the first equation. This part for the right hand side.
     #
-    vh[2] = vh[2] - ei*grid.sqr[1]^k21 * ( c2*(grid.r2[2] - grid.r2[1]) + 
-            c3*(grid.r[2]^3 - grid.r[1]^3) )
+    vh[2] = vh[2] - ei*sqr[1]^k21 * ( c2*(r2[2] - r2[1]) + c3*(r[2]^3 - r[1]^3) )
     
     #
     # solve the linear system with lapack routine dptsv
     #
     # XXX: Use \ ?
     #call dptsv(mesh-2,1,d(2),e(2),vh(2),mesh-2,ierr)
-    #if (ierr.ne.0) call upf_error('hartree', 'error in lapack', ierr)
-  
+    @views LAPACK.ptsv!(d[2:Nrmesh-1], e[2:Nrmesh-2], vh[2:Nrmesh-1])
+
     # Set the value of the solution at the first and last point
     # First, find c0 from the solution in the second point
-    c0 = vh[2]/grid.sqr[2]^k21 - c2*grid.r2[2] - c3*grid.r[2]^2
+    c0 = vh[2]/sqr[2]^k21 - c2*r2[2] - c3*r[2]^2
   
     # and then use the series expansion at the first point
-    vh[1] = grid.sqr[1]^k21 * ( c0 + c2*grid.r2[1] + c3*grid.r[1]^3 )
+    vh[1] = sqr[1]^k21 * ( c0 + c2*r2[1] + c3*r[1]^3 )
 
     # the solution at the last point is given  by the boundary 
     # condition
     vh[Nrmesh] = vh[Nrmesh-1]*fn
 
     # The solution must be divided by r (from the equation) 
-    # and multiplied by the square root of r (from the log  mesh transformation)
+    # and multiplied by the square root of r (from the log mesh transformation)
     for i in 1:Nrmesh
-       vh[i] = vh[i] / grid.sqr[i]
+       vh[i] = vh[i] / sqr[i]
     end
 
   return
