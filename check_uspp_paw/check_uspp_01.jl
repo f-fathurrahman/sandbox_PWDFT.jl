e1659ff815a1fb9d7cadc58dd152af07b0a8ca72
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-06-11 16:16:30 +0700

    Prepare Rhoe and potential

--- check_uspp_paw/check_uspp_01.jl
+++ check_uspp_paw/check_uspp_01.jl
@@ -20,6 +20,16 @@ include(joinpath(DIR_PWDFT, "utilities", "init_Ham_from_pwinput.jl"))
 
 function create_Ham(filename)
     Ham, pwinput = init_Ham_from_pwinput(filename=filename);
+    # Initial density
+    Rhoe, RhoeG = atomic_rho_g(Ham);
+    # Also initialize becsum in case of PAW
+    if any(Ham.pspotNL.are_paw)
+        PAW_atomic_becsum!(Ham);
+    end
+    # Update the potentials
+    Ehartree, Exc, Evtxc = update_from_rhoe!( Ham, nothing, Rhoe, RhoeG )
+    Ham.energies.Hartree = Ehartree
+    Ham.energies.XC = Exc
     return Ham
 end
 
@@ -59,6 +69,169 @@ function load_qradG()
     return qradG
 end
 
+function load_qq_at_nt()
+    json_data = JSON.parsefile("uspp_mod.json")["uspp"]
+    
+    shape_qq_nt = convert(Vector{Int64}, json_data["shape_qq_nt"])
+    qq_nt = reshape(
+        convert(Vector{Float64}, json_data["qq_nt"]),
+        shape_qq_nt...
+    )
+
+    shape_qq_at = convert(Vector{Int64}, json_data["shape_qq_at"])
+    qq_at = reshape(
+        convert(Vector{Float64}, json_data["qq_at"]),
+        shape_qq_at...
+    )
+
+    return qq_at, qq_nt
+end
+
+
+
+function check_qq_at_qq_nt(Ham::Hamiltonian)
+
+    if !any(Ham.pspotNL.are_ultrasoft)
+        println("No USPP or PAW data: skipped")
+        return
+    end
+
+    println()
+    println("------------------------")
+    println("Checking qq_at and qq_nt")
+    println("------------------------")
+
+    Q_CONV_FACTOR = 4
+
+    qq_at, qq_nt = load_qq_at_nt()
+
+    println()
+    println("qq_at")
+    diff1 = Ham.pspotNL.qq_at - qq_at*Q_CONV_FACTOR
+    println("maximum abs diff = ", maximum(abs.(diff1)))
+    println("minimum abs diff = ", minimum(abs.(diff1)))
+    println("mean    abs diff = ", mean(abs.(diff1)))
+
+    println()
+    println("qq_nt")
+    diff1 = Ham.pspotNL.qq_nt - qq_nt*Q_CONV_FACTOR
+    println("maximum abs diff = ", maximum(abs.(diff1)))
+    println("minimum abs diff = ", minimum(abs.(diff1)))
+    println("mean    abs diff = ", mean(abs.(diff1)))
+end
+
+
+function load_dvan()
+    json_data = JSON.parsefile("uspp_mod.json")["uspp"]
+    shape_dvan = convert(Vector{Int64}, json_data["shape_dvan"])
+    dvan = reshape(
+        convert(Vector{Float64}, json_data["dvan"]),
+        shape_dvan...
+    )
+    return dvan
+end
+
+function check_Dvan(Ham::Hamiltonian)
+
+    if !any(Ham.pspotNL.are_ultrasoft)
+        println("No USPP or PAW data: skipped")
+        return
+    end
+
+    println()
+    println("-------------")
+    println("Checking Dvan")
+    println("-------------")
+
+    CONV_FACTOR = 2
+
+    dvan = load_dvan()
+
+    diff1 = Ham.pspotNL.Dvan - dvan*CONV_FACTOR
+    println("maximum abs diff = ", maximum(abs.(diff1)))
+    println("minimum abs diff = ", minimum(abs.(diff1)))
+    println("mean    abs diff = ", mean(abs.(diff1)))
+end
+
+
+function subtract_Dvan(Ham::Hamiltonian)
+
+    Deeq = Ham.pspotNL.Deeq
+    Dvan = Ham.pspotNL.Dvan
+
+    Deeq_new = copy(Deeq)
+    Natoms = Ham.atoms.Natoms
+    atm2species = Ham.atoms.atm2species
+    Nspin = size(Deeq, 4)
+    nh = Ham.pspotNL.nh
+
+    for ia in 1:Natoms
+        isp = atm2species[ia]
+        for ispin in 1:Nspin
+            for ih in 1:nh[isp], jh in ih:nh[isp]
+                Deeq_new[ih,jh,ia,ispin] = Deeq[ih,jh,ia,ispin] - Dvan[ih,jh,isp]
+                Deeq_new[jh,ih,ia,ispin] = Deeq_new[ih,jh,ia,ispin]
+            end
+        end
+    end
+    return Deeq_new
+end
+
+# Deeq and Dvan can be from QE
+function subtract_Dvan(Ham::Hamiltonian, Deeq, Dvan)
+
+    Deeq_new = copy(Deeq)
+    Natoms = Ham.atoms.Natoms
+    atm2species = Ham.atoms.atm2species
+    Nspin = size(Deeq, 4)
+    nh = Ham.pspotNL.nh
+
+    for ia in 1:Natoms
+        isp = atm2species[ia]
+        for ispin in 1:Nspin
+            for ih in 1:nh[isp], jh in ih:nh[isp]
+                Deeq_new[ih,jh,ia,ispin] = Deeq[ih,jh,ia,ispin] - Dvan[ih,jh,isp]
+                Deeq_new[jh,ih,ia,ispin] = Deeq_new[ih,jh,ia,ispin]
+            end
+        end
+    end
+    return Deeq_new
+end
+
+
+
+function load_deeq()
+    json_data = JSON.parsefile("uspp_mod.json")["uspp"]
+    shape_deeq = convert(Vector{Int64}, json_data["shape_deeq"])
+    deeq = reshape(
+        convert(Vector{Float64}, json_data["deeq"]),
+        shape_deeq...
+    )
+    return deeq
+end
+
+
+function check_Deeq(Ham::Hamiltonian)
+
+    if !any(Ham.pspotNL.are_ultrasoft)
+        println("No USPP or PAW data: skipped")
+        return
+    end
+
+    println()
+    println("-------------")
+    println("Checking Deeq")
+    println("-------------")
+
+    CONV_FACTOR = 2
+
+    deeq = load_deeq()
+
+    diff1 = Ham.pspotNL.Deeq - deeq*CONV_FACTOR
+    println("maximum abs diff = ", maximum(abs.(diff1)))
+    println("minimum abs diff = ", minimum(abs.(diff1)))
+    println("mean    abs diff = ", mean(abs.(diff1)))
+end
 
 
 
@@ -114,8 +287,8 @@ end
 
 function load_V_Ps_loc()
     json_data = JSON.parsefile("scf_mod.json")["scf_mod"]
-    vltot = convert(Vector{Float64}, json_data["vltot"])*0.5 # convert to Ha
-    return vltot
+    vltot = convert(Vector{Float64}, json_data["vltot"])
+    return vltot*0.5 # convert to Ha
 end
 
 function check_V_Ps_loc(Ham)
@@ -124,8 +297,8 @@ function check_V_Ps_loc(Ham)
     println("Checking V_Ps_loc")
     println("-----------------")
 
-    vltot_Ha = load_V_Ps_loc()
-    diff1 = Ham.potentials.Ps_loc  - vltot_Ha
+    vltot = load_V_Ps_loc()
+    diff1 = Ham.potentials.Ps_loc - vltot
     println("maximum abs diff = ", maximum(abs.(diff1)))
     println("minimum abs diff = ", minimum(abs.(diff1)))
     println("mean    abs diff = ", mean(abs.(diff1)))
@@ -225,8 +398,69 @@ function check_Vloc_gl_sp(Ham)
     println("maximum abs diff = ", maximum(abs.(diff1)))
     println("minimum abs diff = ", minimum(abs.(diff1)))
     println("mean    abs diff = ", mean(abs.(diff1)))
-
 end
+
+
+function load_vrs()
+    json_data = JSON.parsefile("scf_mod.json")["scf_mod"]
+    shape_vrs = convert(Vector{Int64}, json_data["shape_vrs"])
+    vrs = reshape(
+        convert(Vector{Float64}, json_data["vrs"]),
+        shape_vrs...
+    )
+    return vrs*0.5 # convert to Ha
+end
+
+function load_v_of_r()
+    json_data = JSON.parsefile("scf_mod.json")["scf_mod"]
+    shape_v_of_r = convert(Vector{Int64}, json_data["shape_v_of_r"])
+    v_of_r = reshape(
+        convert(Vector{Float64}, json_data["v_of_r"]),
+        shape_v_of_r...
+    )
+    return v_of_r*0.5 # convert to Ha
+end
+
+
+function check_v_of_r(Ham)
+
+    println()
+    println("---------------")
+    println("Checking v_of_r")
+    println("---------------")
+
+    v_of_r = load_v_of_r()
+    diff1 = (Ham.potentials.XC + Ham.potentials.Hartree) - v_of_r
+    println("maximum abs diff = ", maximum(abs.(diff1)))
+    println("minimum abs diff = ", minimum(abs.(diff1)))
+    println("mean    abs diff = ", mean(abs.(diff1)))
+end
+
+
+function load_rho_of_r()
+    json_data = JSON.parsefile("scf_mod.json")["scf_mod"]
+    shape_rho_of_r = convert(Vector{Int64}, json_data["shape_rho_of_r"])
+    rho_of_r = reshape(
+        convert(Vector{Float64}, json_data["rho_of_r"]),
+        shape_rho_of_r...
+    )
+    return rho_of_r
+end
+
+
+function check_Rhoe(Ham)
+    println()
+    println("-------------")
+    println("Checking Rhoe")
+    println("-------------")
+
+    rho_of_r = load_rho_of_r()
+    diff1 = Ham.rhoe - rho_of_r
+    println("maximum abs diff = ", maximum(abs.(diff1)))
+    println("minimum abs diff = ", minimum(abs.(diff1)))
+    println("mean    abs diff = ", mean(abs.(diff1)))
+end
+
 
 
 include("pseudo_upf.jl")
@@ -244,8 +478,9 @@ function main()
     check_rhoe_core(Ham)
     check_V_Ps_loc(Ham)
     check_Vloc_gl_sp(Ham)
+    check_Rhoe(Ham)
+    check_v_of_r(Ham)
     check_clebsch_gordan(Ham)
-
 
     return
 end
