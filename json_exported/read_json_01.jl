4b9857366f83a549a12632d519bb3b1950e331dc
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-06-16 06:33:37 +0700

    Read more potentials related variables

--- json_exported/read_json_01.jl
+++ json_exported/read_json_01.jl
@@ -32,7 +32,50 @@ using JSON
 json_data = JSON.parsefile("scf_mod.json")["scf_mod"];
 rhoe_core = convert(Vector{Float64}, json_data["rho_core"]);
 
-diff1 = Ham.rhoe_core  - rhoe_core;
+diff1 = Ham.rhoe_core - rhoe_core;
 maximum(abs.(diff1))
 
 vltot = convert(Vector{Float64}, json_data["vltot"]);
+shape_v_of_r = convert(Vector{Int64}, json_data["shape_v_of_r"]);
+v_of_r = reshape(
+    convert(Vector{Float64}, json_data["v_of_r"]),
+    shape_v_of_r...
+);
+
+shape_rho_of_r = convert(Vector{Int64}, json_data["shape_rho_of_r"]);
+rho_of_r = reshape(
+    convert(Vector{Float64}, json_data["rho_of_r"]),
+    shape_rho_of_r...
+);
+
+
+#
+# USPP related variables
+#
+using JSON
+json_data = JSON.parsefile("uspp_mod.json")["uspp"]
+    
+shape_qq_nt = convert(Vector{Int64}, json_data["shape_qq_nt"])
+qq_nt = reshape(
+    convert(Vector{Float64}, json_data["qq_nt"]),
+    shape_qq_nt...
+)
+
+shape_qq_at = convert(Vector{Int64}, json_data["shape_qq_at"])
+qq_at = reshape(
+    convert(Vector{Float64}, json_data["qq_at"]),
+    shape_qq_at...
+)
+
+shape_dvan = convert(Vector{Int64}, json_data["shape_dvan"])
+dvan = reshape(
+    convert(Vector{Float64}, json_data["dvan"]),
+    shape_dvan...
+)
+
+shape_deeq = convert(Vector{Int64}, json_data["shape_deeq"])
+deeq = reshape(
+    convert(Vector{Float64}, json_data["deeq"]),
+    shape_deeq...
+)
+
