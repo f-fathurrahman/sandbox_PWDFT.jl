96cc7346210eb68fcae0c2bcc88237095c7f5a2e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-07-31 15:32:20 +0700

    find_icl and inward integration

--- ld1/main_integ_sch_01.jl
+++ ld1/main_integ_sch_01.jl
@@ -3,12 +3,78 @@ using Plots, PlotThemes
 
 theme(:dark)
 
-function integ_numerov_sch_outward!(
+function find_icl(E::Float64, V::Vector{Float64})
+
+    N = size(V, 1)
+
+    fm1 = V[1] - E
+    if fm1 == 0.0
+        fm1 = 1e-20
+    end
+
+    icl = -1
+    for i in 2:N
+        f = V[i] - E
+        #XXX Do let let f become too small (?)
+        if f == 0.0
+            f = 1e-20
+        end
+        if sign(f*fm1) < 0
+            icl = i
+        end
+        fm1 = f
+    end
+
+    return icl
+end
+
+
+function integ_numerov_sch_inward!(
+    icl::Int64,
     E::Float64,
     V::Vector{Float64},
     Δx::Float64,
     y::Vector{Float64};
-    LARGE=1e5,
+    parity=:odd
+)
+
+
+
+    Nmesh = size(V,1)
+    @assert Nmesh == size(y,1)
+
+    # XXX: Need to calculate beyond icl ?
+    f = zeros(Float64, Nmesh)
+    for i in 1:Nmesh
+        f[i] = 1 + 2*( E - V[i] )*Δx^2/12
+    end
+
+    y[Nmesh] = Δx
+    y[Nmesh-1] = ( 12.0 - 10.0*f[Nmesh] )*y[Nmesh]/f[Nmesh-1]
+    nrm_guard = 1e100
+    for i in range(Nmesh-1,icl+1,-1)
+        y[i-1] = ( (12.0 - 10.0*f[i] )*y[i] - f[i+1]*y[i+1])/f[i-1]
+        # the following lines prevent overflows if starting from too far
+        if abs(y[i-1]) > nrm_guard
+            @views y[i-1:Nmesh] .= y[i-1:Nmesh] / nrm_guard
+        end
+    end
+
+    return
+
+end
+
+
+
+
+
+
+function integ_numerov_sch_outward!(
+    icl::Int64,
+    E::Float64,
+    V::Vector{Float64},
+    Δx::Float64,
+    y::Vector{Float64};
     parity=:odd
 )
 
@@ -16,12 +82,14 @@ function integ_numerov_sch_outward!(
     Nmesh = size(V,1)
     @assert Nmesh == size(y,1)
 
+    # XXX: Need to calculate beyond icl ?
     f = zeros(Float64, Nmesh)
     for i in 1:Nmesh
         f[i] = 1 + 2*( E - V[i] )*Δx^2/12
     end
 
     # Two possible starting points
+    # Is this important (?)
     if parity == :odd
         y[1] = 1.0
         y[2] = ( 12.0 - 10.0*f[1] )*y[1]/(2*f[2])
@@ -33,21 +101,16 @@ function integ_numerov_sch_outward!(
     end
 
     Ncross = 0
-    iend = Nmesh
-    for i in 2:Nmesh-1
+    for i in 2:icl-1
         y[i+1] = ( (12.0 - 10.0*f[i])*y[i] - f[i-1]*y[i-1] )/ f[i+1]
         # Check for changed sign
         if sign(y[i] * y[i+1]) < 0
             println("Found crossing: ", i, "--", i + 1)
             Ncross = Ncross + 1
         end
-        if abs(y[i+1]) > LARGE
-            iend = i + 1
-            break
-        end
     end
 
-    return Ncross, iend
+    return Ncross
 end
 
 
@@ -73,19 +136,20 @@ function main()
     #title!("Harmonic Potential")
     #savefig("IMG_pot.png")
 
-    E = 1.5
+    E = 0.1
     psi = zeros(Float64, Npoints)
+    icl = find_icl(E, Vpot)
+    println("icl = ", icl)
     #Ncross = integ_numerov_sch!( E, Vpot, Δx, psi, parity=:even )
-    Ncross, iend = integ_numerov_sch_outward!( E, Vpot, Δx, psi, parity=:even )
+    Ncross = integ_numerov_sch_outward!( icl, E, Vpot, Δx, psi, parity=:odd )
     println("Ncross = ", Ncross)
-    println("iend = ", iend)
 
     #nrm = sum(psi .* psi) * Δx
     #psi *= (1/sqrt(nrm))
     #nrm = sum(psi .* psi) * Δx
     #println("nrm psi = ", nrm)
 
-    plot(xgrid[1:iend], psi[1:iend], label="psi1", linewidth=2, dpi=200)
+    plot(xgrid[1:icl], psi[1:icl], label="psi1", linewidth=2, dpi=200)
     savefig("IMG_psi1.png")
 
 end
