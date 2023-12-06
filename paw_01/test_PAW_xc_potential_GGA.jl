b539e6758d3aaa9d2a2952f746307cf3577a8c2c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-12-06 21:46:12 +0700

    Rename, split out PAW_xc_potential_GGA

---
+++ paw_01/test_PAW_xc_potential_GGA.jl
@@ -0,0 +1,63 @@
+using Printf
+using OffsetArrays
+using LinearAlgebra
+
+using Random
+Random.seed!(1234)
+
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)), "..")
+
+include(joinpath(DIR_PWDFT, "utilities", "PWSCFInput.jl"))
+include(joinpath(DIR_PWDFT, "utilities", "init_Ham_from_pwinput.jl"))
+
+include("v1_PAW_xc_potential_GGA.jl")
+
+
+
+function main(;filename=nothing)
+    Ham, pwinput = init_Ham_from_pwinput(filename=filename)
+
+    Nspecies = Ham.atoms.Nspecies
+    for isp in 1:Nspecies
+        println(Ham.pspots[isp])
+    end
+
+    becsum = PAW_atomic_becsum(Ham.atoms, Ham.pspots, Ham.pspotNL, Nspin=1)
+    
+    println("sum becsum before PAW_symmetrize: ", sum(becsum))
+    PAW_symmetrize!(Ham, becsum)
+    println("sum becsum after PAW_symmetrize: ", sum(becsum))
+
+    atoms = Ham.atoms
+    pspots = Ham.pspots
+    pspotNL = Ham.pspotNL
+    AE = true
+    ia = 1
+    isp = atoms.atm2species[ia]
+    Nrmesh = pspots[isp].Nr
+    l2 = (pspots[isp].lmax_rho + 1)^2
+    Nspin = 1
+    xc_calc = Ham.xc_calc
+
+    # Calculate rho_lm
+    rho_lm = zeros(Float64, Nrmesh, l2, Nspin)
+    PAW_rho_lm!(AE, ia, atoms, pspots, pspotNL, becsum, rho_lm)
+    println("sum rho_lm = ", sum(rho_lm))
+    v_lm = zeros(Float64, Nrmesh, l2, Nspin)
+
+    energy = v1_PAW_xc_potential_GGA!( AE, ia,
+        atoms, pspots, pspotNL, xc_calc,
+        rho_lm, v_lm
+    )
+
+    println()
+    println("energy for all ix = ", energy)
+    println("sum abs vout_lm = ", sum(abs.(v_lm)))
+
+
+
+end
+
+main()
--- paw_01/v1_PAW_xc_potential_GGA.jl
+++ paw_01/v1_PAW_xc_potential_GGA.jl
@@ -1,18 +1,3 @@
-using Printf
-using OffsetArrays
-using LinearAlgebra
-
-using Random
-Random.seed!(1234)
-
-using PWDFT
-
-const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)), "..")
-
-include(joinpath(DIR_PWDFT, "utilities", "PWSCFInput.jl"))
-include(joinpath(DIR_PWDFT, "utilities", "init_Ham_from_pwinput.jl"))
-
-
 function _driver_xc_PBE!(
     xc_calc::LibxcXCCalculator,
     Rhoe,
@@ -90,7 +75,7 @@ end
 # Use the density produced by sum_rad_rho to compute xc potential
 # and energy, as xc functional is not diagonal on angular momentum
 # numerical integration is performed.
-function PAW_xc_potential_GGA!(
+function v1_PAW_xc_potential_GGA!(
     AE::Bool, ia,
     atoms, pspots, pspotNL,
     xc_calc,
@@ -206,49 +191,3 @@ function PAW_xc_potential_GGA!(
     return energy
 
 end
-
-function main(;filename=nothing)
-    Ham, pwinput = init_Ham_from_pwinput(filename=filename)
-
-    Nspecies = Ham.atoms.Nspecies
-    for isp in 1:Nspecies
-        println(Ham.pspots[isp])
-    end
-
-    becsum = PAW_atomic_becsum(Ham.atoms, Ham.pspots, Ham.pspotNL, Nspin=1)
-    
-    println("sum becsum before PAW_symmetrize: ", sum(becsum))
-    PAW_symmetrize!(Ham, becsum)
-    println("sum becsum after PAW_symmetrize: ", sum(becsum))
-
-    atoms = Ham.atoms
-    pspots = Ham.pspots
-    pspotNL = Ham.pspotNL
-    AE = true
-    ia = 1
-    isp = atoms.atm2species[ia]
-    Nrmesh = pspots[isp].Nr
-    l2 = (pspots[isp].lmax_rho + 1)^2
-    Nspin = 1
-    xc_calc = Ham.xc_calc
-
-    # Calculate rho_lm
-    rho_lm = zeros(Float64, Nrmesh, l2, Nspin)
-    PAW_rho_lm!(AE, ia, atoms, pspots, pspotNL, becsum, rho_lm)
-    println("sum rho_lm = ", sum(rho_lm))
-    v_lm = zeros(Float64, Nrmesh, l2, Nspin)
-
-    energy = PAW_xc_potential_GGA!( AE, ia,
-        atoms, pspots, pspotNL, xc_calc,
-        rho_lm, v_lm
-    )
-
-    println()
-    println("energy for all ix = ", energy)
-    println("sum abs vout_lm = ", sum(abs.(v_lm)))
-
-
-
-end
-
-main()
