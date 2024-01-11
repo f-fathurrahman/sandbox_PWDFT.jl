86f003e561865654043029c34693427e890c4862
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-01-12 05:06:24 +0700

    Implement calc_stress_Ps_loc

--- uspp_01/test_calc_stress_01.jl
+++ uspp_01/test_calc_stress_01.jl
@@ -1,5 +1,6 @@
 using Printf, LinearAlgebra, Serialization
 using FFTW
+using SpecialFunctions: erf
 using PWDFT
 
 const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
@@ -7,6 +8,8 @@ include(joinpath(DIR_PWDFT, "utilities", "PWSCFInput.jl"))
 include(joinpath(DIR_PWDFT, "utilities", "init_Ham_from_pwinput.jl"))
 
 include("calc_stress_hartree.jl")
+include("eval_dVloc_G.jl")
+include("calc_stress_Ps_loc.jl")
 
 function main()
     Ham = Serialization.deserialize("Hamiltonian.dat")
@@ -16,13 +19,23 @@ function main()
 
     stress_hartree = zeros(Float64, 3, 3)
     calc_stress_hartree!( Ham.pw, Ham.rhoe, Ham.energies.Hartree, stress_hartree )
-
     stress_hartree *= 2.0 # convert to Ry
-    println("stress_hartree (in Ry/bohr^3) = ")
+    println("\nstress_hartree (in Ry/bohr^3) = ")
     for i in 1:3
         @printf("%18.10f %18.10f %18.10f\n", stress_hartree[i,1], stress_hartree[i,2], stress_hartree[i,3])
     end
     display(stress_hartree); println()
+
+
+    stress_Ps_loc = zeros(Float64, 3, 3)
+    calc_stress_Ps_loc!( Ham.atoms, Ham.pw, Ham.pspots, Ham.rhoe, stress_Ps_loc )
+    stress_Ps_loc *= 2.0 # convert to Ry
+    println("\nstress_Ps_loc (in Ry/bohr^3) = ")
+    for i in 1:3
+        @printf("%18.10f %18.10f %18.10f\n", stress_Ps_loc[i,1], stress_Ps_loc[i,2], stress_Ps_loc[i,3])
+    end
+    display(stress_Ps_loc); println()
+
 
     return
 end
