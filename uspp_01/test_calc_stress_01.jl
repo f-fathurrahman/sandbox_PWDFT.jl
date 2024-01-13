3a7b43dc2bcc97f3ec82cf7acbe3a48e96b544bb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-01-13 21:35:05 +0700

    Test stress_xc

--- uspp_01/test_calc_stress_01.jl
+++ uspp_01/test_calc_stress_01.jl
@@ -10,6 +10,7 @@ include(joinpath(DIR_PWDFT, "utilities", "init_Ham_from_pwinput.jl"))
 include("calc_stress_hartree.jl")
 include("eval_dVloc_G.jl")
 include("calc_stress_Ps_loc.jl")
+include("calc_stress_xc.jl")
 
 function main()
     Ham = Serialization.deserialize("Hamiltonian.dat")
@@ -24,7 +25,6 @@ function main()
     for i in 1:3
         @printf("%18.10f %18.10f %18.10f\n", stress_hartree[i,1], stress_hartree[i,2], stress_hartree[i,3])
     end
-    display(stress_hartree); println()
 
 
     stress_Ps_loc = zeros(Float64, 3, 3)
@@ -34,7 +34,15 @@ function main()
     for i in 1:3
         @printf("%18.10f %18.10f %18.10f\n", stress_Ps_loc[i,1], stress_Ps_loc[i,2], stress_Ps_loc[i,3])
     end
-    display(stress_Ps_loc); println()
+
+
+    stress_xc = zeros(Float64, 3, 3)
+    calc_stress_xc!( Ham.pw, Ham.potentials, Ham.rhoe, Ham.energies.XC, stress_xc )
+    stress_xc *= 2.0 # convert to Ry
+    println("\nstress_xc (in Ry/bohr^3) = ")
+    for i in 1:3
+        @printf("%18.10f %18.10f %18.10f\n", stress_xc[i,1], stress_xc[i,2], stress_xc[i,3])
+    end
 
 
     return
