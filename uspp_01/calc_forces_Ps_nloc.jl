4a84211a9dcfc3c70f9efd5d946558cda644951e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-01-16 06:22:58 +0700

    Separate calc_Deff

--- uspp_01/calc_forces_Ps_nloc.jl
+++ uspp_01/calc_forces_Ps_nloc.jl
@@ -1,3 +1,6 @@
+include("calc_Deff.jl")
+
+
 function my_calc_forces_Ps_nloc!(
     atoms::Atoms,    
     pw::PWGrid,
@@ -127,40 +130,6 @@ function _force_Ps_nloc_k!(ipol, ik, ispin,
     end
     return
 end
-
-
-
-
-
-
-function _calc_Deff!(
-    ispin::Int64, atoms, pspotNL, ebnd::Float64, Deff
-)
-    Natoms = atoms.Natoms
-    Nspecies = atoms.Nspecies
-    atm2species = atoms.atm2species
-    Deeq = pspotNL.Deeq
-    qq_at = pspotNL.qq_at
-    #
-    ok_uspp_or_paw = any(pspotNL.are_ultrasoft) || any(pspotNL.are_paw)
-    #
-    @views Deff[:,:,:] .= Deeq[:,:,:,ispin]
-    #println("ok_uspp_or_paw = ", ok_uspp_or_paw)
-    #
-    if ok_uspp_or_paw
-        for isp in 1:Nspecies, ia in 1:Natoms
-            if isp != atm2species[ia]
-                continue
-            end
-            Deff[:,:,ia] .-= ebnd * qq_at[:,:,ia]
-        end
-    end
-    #println("sum qq_at = ", sum(qq_at))
-    #println("sum Deeq = ", sum(Deeq))
-    #println("sum Deff = ", sum(Deff))
-    return
-end
-
 
 
 # This routine computes the contribution to atomic forces due
