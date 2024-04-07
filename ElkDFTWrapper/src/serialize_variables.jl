9b1b8182793f58bff415fe22106ea146a7f12b90
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-04-07 08:47:07 +0700

    Rename

--- ElkDFTWrapper/src/serialize_variables.jl
+++ ElkDFTWrapper/src/serialize_variables.jl
@@ -1,12 +1,12 @@
 """
 Serialize some Elk's global variables using Julia's `serialize`.
 """
-function write_elk_variables()
+function serialize_variables()
     # Muffin tin charge density
-    rhomt = elk_get_rhomt()
+    rhomt = get_rhomt()
     serialize(joinpath(ELK_DATA_DIR, "rhomt.dat"), rhomt)
     #
-    rhoir = elk_get_rhoir()
+    rhoir = get_rhoir()
     serialize(joinpath(ELK_DATA_DIR, "rhoir.dat"), rhoir)
     return
 end
