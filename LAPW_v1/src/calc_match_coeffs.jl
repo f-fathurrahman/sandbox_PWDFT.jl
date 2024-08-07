05b1a34413bd6cf652c69440ae0c8efe386213d7
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-07 12:33:13 +0700

    Draft calc match coeffs

--- LAPW_v1/src/calc_match_coeffs.jl
+++ LAPW_v1/src/calc_match_coeffs.jl
@@ -1,13 +1,3 @@
-#=
-!INPUT/OUTPUT PARAMETERS:
-!
-!   ngp    : number of G+p-vectors (in,integer)
-!   vgpc   : G+p-vectors in Cartesian coordinates (in,real(3,ngkmax))
-!   gpc    : length of G+p-vectors (in,real(ngkmax))
-!   sfacgp : structure factors of G+p-vectors (in,complex(ngkmax,natmtot))
-!   apwalm : APW matching coefficients
-!            (out,complex(ngkmax,apwordmax,lmmaxapw,natmtot))
-=#
 function calc_match_coeffs!(ngp, vgpc, gpc, sfacgp, apwalm)
     #=
   ! arguments
@@ -31,111 +21,107 @@ function calc_match_coeffs!(ngp, vgpc, gpc, sfacgp, apwalm)
   REAL(8) polynm
   external polynm
 =#
+    djl = OffsetArray(
+        zeros(Float64, lmaxapw+1, apwordmax, ngp),
+        0:lmaxapw, 1:apwordmax, 1:ngp
+    )
+    ylmgp = zeros(ComplexF6, lmmaxapw, ngp)
 
+    if apwordmax > 1
+        b = zeros(ComplexF64, apwordmax, ngp*(2*lmaxapw+1))
+    end
+  
+    # compute the spherical harmonics of the G+p-vectors
+    for igp in 1:ngp
+        @views genylmv!( lmaxapw, vgpc[:,igp], ylmgp[:,igp] )
+    end
 
-  ALLOCATE(djl(0:lmaxapw,apwordmax,ngp),ylmgp(lmmaxapw,ngp))
-  IF(apwordmax > 1) ALLOCATE(b(apwordmax,ngp*(2*lmaxapw+1)))
+    t0 = 4π/sqrt(CellVolume)
   
-  ! compute the spherical harmonics of the G+p-vectors
-  DO igp=1,ngp
-    CALL genylmv(lmaxapw,vgpc(:,igp),ylmgp(:,igp))
-  ENDDO 
-  t0=fourpi/sqrt(omega)
-  
-  ! loop over species
-  DO is=1,nspecies
-    nr=nrmt(is)
-    ! maximum APW order for this species
-    omax=maxval(apword(1:lmaxapw,is))
-    ! special case of omax=1
-    IF(omax==1) THEN 
-      DO igp=1,ngp
-        t1=gpc(igp)*rmt(is)
-        CALL sbessel(lmaxapw,t1,djl(:,1,igp))
-      ENDDO 
-      DO ia=1,natoms(is)
-        ias=idxas(ia,is)
-        DO l=0,lmaxapw
-          z1=(t0/apwfr(nr,1,1,l,ias))*zil(l)
-          DO igp=1,ngp
-            z2=djl(l,1,igp)*z1*sfacgp(igp,ias)
-            DO m=-l,l
-              lm=idxlm(l,m)
-              apwalm(igp,1,lm,ias)=z2*conjg(ylmgp(lm,igp))
-            ENDDO 
-          ENDDO 
-        ENDDO 
-      ENDDO 
-      CYCLE 
-    ENDIF 
-    ! starting point on radial mesh for fitting polynomial of order npapw
-    ir=nr-npapw+1
-    ! evaluate the spherical Bessel function derivatives for all G+p-vectors
-    DO igp=1,ngp
-      t1=gpc(igp)*rmt(is)
-      DO io=1,omax
-        CALL sbesseldm(io-1,lmaxapw,t1,djl(:,io,igp))
-      ENDDO 
-      t1=1.d0
-      DO io=2,omax
-        t1=t1*gpc(igp)
-        djl(:,io,igp)=t1*djl(:,io,igp)
-      ENDDO 
-    ENDDO 
-    ! loop over atoms
-    DO ia=1,natoms(is)
-      ias=idxas(ia,is)
-      ! begin loop over l
-      DO l=0,lmaxapw
-        z1=t0*zil(l)
-        ! set up matrix of derivatives
-        DO jo=1,apword(l,is)
-          DO io=1,apword(l,is)
-            a(io,jo)=polynm(io-1,npapw,rsp(ir,is),apwfr(ir,1,jo,l,ias),rmt(is))
-          ENDDO 
-        ENDDO 
-        ! set up target vectors
-        i=0
-        DO igp=1,ngp
-          z2=z1*sfacgp(igp,ias)
-          DO m=-l,l
-            lm=idxlm(l,m)
-            i=i+1
-            z3=z2*conjg(ylmgp(lm,igp))
-            DO io=1,apword(l,is)
-              b(io,i)=djl(l,io,igp)*z3
-            ENDDO 
-          ENDDO 
-        ENDDO 
-        ! solve the general complex linear systems
-        CALL zgesv(apword(l,is),i,a,apwordmax,ipiv,b,apwordmax,info)
-        IF(info.ne.0) THEN 
-          WRITE(*,*)
-          WRITE(*,'("Error(match): could not find APW matching coefficients")')
-          WRITE(*,'(" for species ",I4," and atom ",I4)') is,ia
-          WRITE(*,'(" ZGESV RETURN ed INFO = ",I8)') info
-          WRITE(*,*)
-          stop
-        ENDIF 
-        i=0
-        DO igp=1,ngp
-          DO m=-l,l
-            lm=idxlm(l,m)
-            i=i+1
-            DO io=1,apword(l,is)
-              apwalm(igp,io,lm,ias)=b(io,i)
-            ENDDO 
-          ENDDO 
-        ENDDO 
-        ! end loop over l
-      ENDDO 
-      ! end loops over atoms and species
-    ENDDO 
-  ENDDO 
-  DEALLOCATE(djl,ylmgp)
+    # loop over species
+    for isp in 1:Nspecies
+        nr = nrmt(is)
+        # maximum APW order for this species
+        omax = maximsum( specs_info[isp].apword )
+        # special case of omax=1
+        if omax==1 
+            for igp in 1:ngp
+                t1 = gpc[igp]*rmt[isp]
+                @views sbessel!(lmaxapw, t1, djl[:,1,igp] )
+            end
+            for ia in 1:Natoms
+                if atm2species[ia] != isp
+                    continue
+                end
+                for l in 0:lmaxapw
+                    z1=(t0/apwfr(nr,1,1,l,ias))*zil(l)
+                    for igp in 1:ngp
+                        z2 = djl[l,1,igp] * z1 * sfacgp[igp,ia]
+                        for m in -l:l
+                            lm = idxlm(l,m)
+                            apwalm[ia][igp,1,lm] = z2*conjg(ylmgp[lm,igp])
+                        end 
+                    end 
+                end 
+            end 
+            continue # CYCLE  # next species
+        end # if omax == 1 
+        # starting point on radial mesh for fitting polynomial of order npapw
+        ir = nr-npapw+1
+        # evaluate the spherical Bessel function derivatives for all G+p-vectors
+        for igp in 1:ngp
+            t1 = gpc(igp)*rmt(is)
+            for io in 1:omax
+                @views sbesseldm!( io-1, lmaxapw, t1, djl[:,io,igp] )
+            end 
+            t1 = 1.0
+            for io in 2:omax
+                t1 = t1*gpc[igp]
+                djl[:,io,igp] = t1*djl[:,io,igp]
+            end 
+        end 
+        # loop over atoms
+        for ia in 1:Natoms
+            if atm2species[ia] != isp
+                continue
+            end
+            # begin loop over l
+            for l in 0:lmaxapw
+                z1 = t0*im^l  #zil[l]
+                # set up matrix of derivatives
+                for jo in 1:apword[isp][l], io in 1:apword[isp][l]
+                    a(io,jo)=polynm(io-1,npapw,rsp(ir,is),apwfr(ir,1,jo,l,ias),rmt(is))
+                end 
+                # set up target vectors
+                i = 0
+                for igp in 1:ngp
+                    z2=z1*sfacgp(igp,ias)
+                    for m in -l:l
+                        lm = idxlm(l,m)
+                        i = i + 1
+                        z3 = z2*conjg(ylmgp[lm,igp])
+                        for io in 1:apword[isp][l]
+                            b[io,i] = djl[l,io,igp]*z3
+                        end 
+                    end 
+                end 
+                # solve the general complex linear systems
+                #CALL zgesv(apword(l,is),i,a,apwordmax,ipiv,b,apwordmax,info)
+                #
+                i = 0
+                for igp in 1:ngp
+                    for m in -l:l
+                        lm = idxlm[l,m]
+                        i = i + 1
+                        for io in 1:apword[isp][l]
+                            apwalm[ia][igp,io,lm] = b[io,i]
+                        end
+                    end 
+                end 
+            end  # end loop over l 
+        end 
+    end # end loops over atoms and species
 
-  IF(apwordmax > 1) DEALLOCATE(b)
-  
-  RETURN 
+    return
 end
 
