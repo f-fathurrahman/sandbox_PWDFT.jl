afb0581158beecf0f7fcebd22ba7200c811a78b7
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-08-20 23:21:08 +0700

    Test accessing exposed variables

--- MinipwWrapper/src/variables.jl
+++ MinipwWrapper/src/variables.jl
@@ -26,3 +26,19 @@ function get_uspp_param_nh()
     # Int64 will be read as Int32, then converted to Int64 again
 end
 
+function get_lsda_nspin()
+    return unsafe_load(cglobal((:__lsda_mod_MOD_nspin, LIBMINIPW), Int32)) |> Int64
+end
+
+function get_dfftp_nnr()
+    ccall((:__exposed_MOD_expose_dfftp_nnr, LIBMINIPW), Cvoid, ())
+    return unsafe_load(cglobal((:__exposed_MOD_dfftp_nnr, LIBMINIPW), Int32)) |> Int64
+end
+
+function get_rho_of_r()
+    symbol = :__exposed_MOD_rho_of_r
+    nnr = get_dfftp_nnr()
+    nspin = get_lsda_nspin()
+    ccall((:__exposed_MOD_expose_rho_of_r, LIBMINIPW), Cvoid, ())
+    return _load_allocatable_array(symbol, Float64, (nnr,nspin))
+end
