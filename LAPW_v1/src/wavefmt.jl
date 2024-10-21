e1c788bb350c6e59569ab78be75220a5fa843c20
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2024-10-21 20:06:14 +0700

    Draft

--- LAPW_v1/src/wavefmt.jl
+++ LAPW_v1/src/wavefmt.jl
@@ -27,73 +27,81 @@ function wavefmt(lrstp, ia, ngp, apwalm, evecfv, wfmt)
     iro = nrmti[isp] + lrstp
 
     if lrstp == 1
-        nrc = nrmt(is)
-        nrci = nrmti(is)
-        npc = npmt(is)
-        npci = npmti(is)
+        # not using coarse vs fine grid
+        # XXX Are there any use cases for this?
+        # XXX Probably for testing
+        nrc = nrmt[isp]
+        nrci = nrmti[isp]
+        npc = npmt[isp]
+        npci = npmti[isp]
     elseif lrstp == lradstp
-        nrc = nrcmt(is)
-        nrci = nrcmti(is)
-  npc=npcmt(is)
-  npci=npcmti(is)
-else
-  write(*,*)
-  write(*,'("Error(wavefmt): invalid lrstp : ",I8)') lrstp
-  write(*,*)
-  stop
-end if
-nrco = nrc - nrci
+        nrc = nrcmt[isp]
+        nrci = nrcmti[isp]
+        npc = npcmt[isp]
+        npci = npcmti[isp]
+    else
+        error("Invalid lrstp = $(lrstp)")
+    end
+    nrco = nrc - nrci
 
-! zero the wavefunction
-wfmt(:,1:npc)=0.d0
+    # zero the wavefunction
+    @views wfmt[:,1:npc] .= 0.0
 
-!-----------------------!
-!     APW functions     !
-!-----------------------!
-lm=0
-do l=0,lmaxo
-  do m=-l,l
-    lm=lm+1
-    i=npci+lm
-    do io=1,apword(l,is)
-      z1=zdotu(ngp,evecfv,1,apwalm(:,io,lm),1)
-      if (abs(dble(z1)) > 1.d-14) then
-        if (l <= lmaxi) then
-          call daxpy(nrci,dble(z1),apwfr(1,1,io,l,ias),lrstp,wfmt(1,lm),ldi)
-        end if
-        call daxpy(nrco,dble(z1),apwfr(iro,1,io,l,ias),lrstp,wfmt(1,i),ldo)
-      end if
-      if (abs(aimag(z1)) > 1.d-14) then
-        if (l <= lmaxi) then
-          call daxpy(nrci,aimag(z1),apwfr(1,1,io,l,ias),lrstp,wfmt(2,lm),ldi)
-        end if
-        call daxpy(nrco,aimag(z1),apwfr(iro,1,io,l,ias),lrstp,wfmt(2,i),ldo)
-      end if
-    end do
-  end do
-end do
-!---------------------------------!
-!     local-orbital functions     !
-!---------------------------------!
-do ilo=1,nlorb(is)
-  l=lorbl(ilo,is)
-  do m=-l,l
-    lm=idxlm(l,m)
-    i=npci+lm
-    z1=evecfv(ngp+idxlo(lm,ilo,ias))
-    if (abs(dble(z1)) > 1.d-14) then
-      if (l <= lmaxi) then
-        call daxpy(nrci,dble(z1),lofr(1,1,ilo,ias),lrstp,wfmt(1,lm),ldi)
-      end if
-      call daxpy(nrco,dble(z1),lofr(iro,1,ilo,ias),lrstp,wfmt(1,i),ldo)
-    end if
-    if (abs(aimag(z1)) > 1.d-14) then
-      if (l <= lmaxi) then
-        call daxpy(nrci,aimag(z1),lofr(1,1,ilo,ias),lrstp,wfmt(2,lm),ldi)
-      end if
-      call daxpy(nrco,aimag(z1),lofr(iro,1,ilo,ias),lrstp,wfmt(2,i),ldo)
-    end if
-  end do
-end do
-return
-end subroutine
+    #-----------------------
+    #     APW functions     
+    #-----------------------
+    lm = 0
+    for l in 0:lmaxo
+        for m in -l:l
+            lm = lm + 1
+            i = npci + lm
+            for io in 1:apword[isp][l]
+                z1 = BLAS.dotu(ngp, evecfv, 1, apwalm[:,io,lm], 1)
+                if abs(real(z1)) > 1.e-14
+                    if l <= lmaxi
+                        #call daxpy(nrci,dble(z1),apwfr(1,1,io,l,ias),lrstp,wfmt(1,lm),ldi)
+                        axpy()
+                    end
+                    #call daxpy(nrco,dble(z1),apwfr(iro,1,io,l,ias),lrstp,wfmt(1,i),ldo)
+                    axpy()
+                end
+                #
+                if abs(imag(z1)) > 1.e-14
+                    if l <= lmaxi
+                        #call daxpy(nrci,aimag(z1),apwfr(1,1,io,l,ias),lrstp,wfmt(2,lm),ldi)
+                    end
+                    #call daxpy(nrco,aimag(z1),apwfr(iro,1,io,l,ias),lrstp,wfmt(2,i),ldo)
+                    axpy()
+                end
+            end
+        end
+    end
+    #---------------------------------
+    #     local-orbital functions     
+    #---------------------------------
+    for ilo in 1:nlorb[isp]
+        l = lorbl[isp][ilo]
+        for m in -l:l
+            lm = idxlm[l,m]
+            i = npci + lm
+            z1 = evecfv[ngp+idxlo[ia][lm,ilo]]
+            if abs(real(z1)) > 1e-14
+                if l <= lmaxi
+                    #call daxpy(nrci,dble(z1),lofr(1,1,ilo,ias),lrstp,wfmt(1,lm),ldi)
+                    axpy()
+                end
+                #call daxpy(nrco,dble(z1),lofr(iro,1,ilo,ias),lrstp,wfmt(1,i),ldo)
+                axpy()
+            end
+            if abs(aimag(z1)) > 1e-14
+                if l <= lmaxi
+                    #call daxpy(nrci,aimag(z1),lofr(1,1,ilo,ias),lrstp,wfmt(2,lm),ldi)
+                    axpy()
+                end
+                #call daxpy(nrco,aimag(z1),lofr(iro,1,ilo,ias),lrstp,wfmt(2,i),ldo)
+                axpy()
+            end
+        end
+    end
+    return
+end
