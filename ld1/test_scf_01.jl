328023b2c972a95be92850400ea7707b238fd81a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-03-31 17:58:35 +0700

    Initial version of scf, need to set xc explicitly

---
+++ ld1/test_scf_01.jl
@@ -0,0 +1,159 @@
+using Revise, Infiltrator
+using Printf
+import LinearAlgebra
+
+# Use import to avoid potential name clash
+import Plots, PlotThemes
+Plots.theme(:dark)
+
+using PWDFT
+
+
+includet("RadialGrid.jl")
+includet("starting_potential.jl")
+includet("start_scheq.jl")
+includet("ascheq.jl")
+includet("lschps.jl")
+includet("radial_poisson_solve.jl")
+
+function test_scf_01()
+
+    Zval = 14.0
+    Zed = Zval # no need to convert it to Ry, how about the sign?
+    Nspin = 1
+    Nwf = 5
+    nn = [1, 2, 2, 3, 3]
+    ll = [0, 0, 1, 0, 1] 
+    oc = [2.0, 2.0, 6.0, 2.0, 2.0]
+    enl = zeros(Float64, Nwf)
+    # FIXME: define isw: spin index
+
+    @assert length(nn) == Nwf
+    @assert length(ll) == Nwf
+    @assert length(oc) == Nwf
+
+    rmax = 100.0
+    xmin = -8.0 # iswitch=1, rel=1
+    #xmin = -7.0 # iswitch=1, rel=0
+    dx = 0.008 # iswitch=1, rel=1
+    ibound = false # default
+
+    # Initialize radial grid
+    grid = RadialGrid(rmax, Zval, xmin, dx, ibound)
+    println("Nrmesh = ", grid.Nrmesh)
+
+    Nrmesh = grid.Nrmesh
+    v0 = zeros(Float64, Nrmesh)
+    vxt = zeros(Float64, Nrmesh)
+    Vpot = zeros(Float64, Nrmesh, Nspin)
+    
+    Rhoe = zeros(Float64, Nrmesh, Nspin)
+    Rhoe_radial = zeros(Float64, Nrmesh, Nspin)
+    V_h = zeros(Float64, Nrmesh)
+    Vxc = zeros(Float64, Nrmesh, Nspin)
+    VHxc = zeros(Float64, Nrmesh, Nspin)
+    VHxc_new = zeros(Float64, Nrmesh, Nspin)
+    epsxc = zeros(Float64, Nrmesh)
+    Vnew = zeros(Float64, Nrmesh, Nspin)
+    psi = zeros(Float64, Nrmesh, Nwf) # spin index dropped for the moment
+    
+    xc_calc = LibxcXCCalculator() # default using VWN
+    ispin = 1
+    
+    starting_potential!(
+        Nrmesh, Zval, Zed,
+        Nwf, oc, nn, ll,
+        grid.r, enl, v0, vxt, Vpot
+    )
+    if Nspin == 2
+        # XXX Same starting potential for spinpol case
+        for i in 1:Nrmesh
+           Vpot[i,2] = Vpot[i,1]
+        end
+    end
+    # Define VHxc input
+    for i in 1:Nrmesh
+        VHxc[i,ispin] = Vpot[i,ispin] + Zed/grid.r[i]
+    end
+
+
+    # Solve for all states
+    thresh0 = 1.0e-10
+    nstop = 0
+    mode = 1 # for lschps
+    ze2 = -Zval # should be 2*Zval in Ry unit
+
+    diff_V = Inf
+
+    for iterSCF in 1:50
+
+        println("\niterSCF = ", iterSCF)
+
+        for iwf in 1:Nwf
+            @views psi1 = psi[:,iwf] # zeros wavefunction
+            enl[iwf], nstop = lschps!( mode, Zval, thresh0, 
+                grid, nn[iwf], ll[iwf], enl[iwf], Vpot, psi1
+            )   
+            #enl[iwf], nstop = ascheq!(
+            #    nn[iwf], ll[iwf], enl[iwf], grid, Vpot, ze2, thresh0, psi1, nstop
+            #)
+        end
+
+        println("Energy levels:")
+        for iwf in 1:Nwf
+            @printf("%3d %18.10f\n", iwf, enl[iwf])
+        end
+
+        #
+        # calculate charge density (spherical approximation)
+        #
+        fill!(Rhoe, 0.0)
+        for iwf in 1:Nwf, ir in 1:Nrmesh
+            # this is for ispin=1
+            Rhoe[ir,ispin] += oc[iwf] * psi[ir,iwf]^2
+        end
+        integRho = PWDFT.integ_simpson(Nrmesh, Rhoe[:,1], grid.rab) 
+        println("integRho = ", integRho)
+
+        radial_poisson_solve!(0, 2, grid, Rhoe, V_h)
+        println("sum V_h = ", sum(V_h))
+
+        Rhoe_radial[:] .= Rhoe[:] ./ grid.r2[:] ./ (4π) # 
+
+        calc_epsxc_Vxc_VWN!(
+            xc_calc, Rhoe_radial,
+            epsxc,
+            Vxc
+        )
+        println("sum Rhoe_radial = ", sum(Rhoe_radial))
+        println("sum epsxc = ", sum(epsxc))
+        println("sum Vxc = ", sum(Vxc))
+
+        for i in 1:Nrmesh
+            VHxc_new[i,ispin] = V_h[i] + Vxc[i,ispin]
+            Vnew[i,ispin] = -Zed/grid.r[i] + vxt[i] + VHxc_new[i,ispin]
+        end
+
+        diff_V = LinearAlgebra.norm(VHxc .- VHxc_new)
+        println("diff_V = ", diff_V)
+        if diff_V < 1e-10
+            println("!!!! CONVERGED !!!")
+            break
+        end
+
+        for i in 1:Nrmesh
+            # Mix
+            VHxc[i,ispin] = 0.5*VHxc_new[i,ispin] + 0.5*VHxc[i,ispin]
+            # Set new potential
+            Vpot[i,ispin] = -Zed/grid.r[i] + VHxc[i,ispin]
+        end
+
+    end
+
+    #@infiltrate
+
+    return
+
+end
+
+test_scf_01()
