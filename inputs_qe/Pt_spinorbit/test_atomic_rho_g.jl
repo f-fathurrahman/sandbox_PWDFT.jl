687c9a6773a0adab24b7d2a821011ca0b2bf4ab1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2026-01-08 06:21:59 +0700

    Update

--- inputs_qe/Pt_spinorbit/test_atomic_rho_g.jl
+++ inputs_qe/Pt_spinorbit/test_atomic_rho_g.jl
@@ -2,9 +2,11 @@ using PWDFT
 
 Ham, pwinput = init_Ham_from_pwinput(filename="PWINPUT_oncv");
 
-# FIXME: need to explicitly set starting_magnetization, angle1, and angle2 to zeros
-# This is the same as domag=false in pw.x
-Rhoe, _ = atomic_rho_g( Ham, starting_magnetization=[0.0], angle1=[0.0], angle2=[0.0] );
+Rhoe, _ = atomic_rho_g( Ham,
+    starting_magn = Ham.options.starting_magn,
+    angle1 = Ham.options.angle1,
+    angle2 = Ham.options.angle2 
+);
 
 psiks = rand_BlochWavefunc(Ham);
 update_from_rhoe!(Ham, psiks, Rhoe);
--- inputs_qe/Pt_spinorbit/test_op_H_01.jl
+++ inputs_qe/Pt_spinorbit/test_op_H_01.jl
@@ -4,8 +4,13 @@ Ham, pwinput = init_Ham_from_pwinput(filename="PWINPUT_oncv");
 #Ham, pwinput = init_Ham_from_pwinput(filename="PWINPUT_oncv_nospinorb");
 
 psiks = rand_BlochWavefunc(Ham);
-Rhoe, _ = atomic_rho_g( Ham, starting_magnetization=[0.0], angle1=[0.0], angle2=[0.0] );
+Rhoe, _ = atomic_rho_g( Ham,
+    starting_magn = Ham.options.starting_magn,
+    angle1 = Ham.options.angle1,
+    angle2 = Ham.options.angle2 
+);
 update_from_rhoe!(Ham, psiks, Rhoe);
 
 K_psiks = op_K(Ham, psiks);
 Vloc_psiks = op_V_loc(Ham, psiks);
+Vpsnloc_psiks = op_V_Ps_nloc(Ham, psiks);
