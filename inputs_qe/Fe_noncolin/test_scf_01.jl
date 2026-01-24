10af2fda9dde5615ced6ae21bfd4f55e87cc71f3
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2026-01-24 21:13:54 +0700

    Script

---
+++ inputs_qe/Fe_noncolin/test_scf_01.jl
@@ -0,0 +1,23 @@
+using Revise, PWDFT
+
+atoms = Atoms(1, 1, [0.0; 0.0; 0.0;;], [1], ["Fe"], ["Fe"],
+    [2.6085 -2.6085 -2.6085; 2.6085 2.6085 -2.6085; 2.6085 2.6085 2.6085], [16.0], [0.0]);
+pspfiles = ["../pseudo_LDA\\Fe_oncv.UPF"];
+ecutwfc = 25.0;
+options = HamiltonianOptions(4.0, 1, 4, [6, 6, 6], [0, 0, 0], false, (0, 0, 0),
+    nothing, "", "VWN", false, -1, 24, true, true, 0.005, [0.5], [90.0], [0.0], false, true);
+pspots = Vector{PsPot_UPF}(undef, atoms.Nspecies);
+for isp in 1:atoms.Nspecies
+    pspots[isp] = PsPot_UPF(pspfiles[isp]);
+end
+Ham = Hamiltonian(atoms, pspots, ecutwfc, options);
+
+psiks = rand_BlochWavefunc(Ham);
+electrons_scf_G!(
+    Ham,
+    psiks = psiks,
+    NiterMax = 10,
+    betamix = 0.1,
+    starting_magn = [0.2],
+    print_final_ebands = false
+)
--- inputs_qe/Pt_spinorbit/test_scf_01.jl
+++ inputs_qe/Pt_spinorbit/test_scf_01.jl
@@ -1,17 +1,14 @@
 using PWDFT
 
-Ham, pwinput = init_Ham_from_pwinput(filename="PWINPUT_oncv");
-
-if pwinput.occupations == "smearing"
-    Ham.electrons.use_smearing = true
-    Ham.electrons.kT = pwinput.degauss*0.5 # convert from Ry to Ha
+atoms = Atoms(1, 1, [0.0; 0.0; 0.0;;], [1], ["Pt"], ["Pt"], [-3.71 0.0 -3.71; 0.0 3.71 3.71; 3.71 3.71 0.0], [18.0], [0.0]);
+pspfiles = ["../pseudo_LDA\\Pt_LDA_FR.SG15v1.2.UPF"];
+ecutwfc = 15.0;
+options = HamiltonianOptions(4.0, 1, 4, [4, 4, 4], [0, 0, 0], false, (0, 0, 0), nothing, "", "VWN", false, -1, 26, true, true, 0.005, [0.0], nothing, nothing, true, true);
+pspots = Vector{PsPot_UPF}(undef, atoms.Nspecies);
+for isp in 1:atoms.Nspecies
+    pspots[isp] = PsPot_UPF(pspfiles[isp]);
 end
-
-if pwinput.nspin == 2
-    starting_magn = pwinput.starting_magnetization
-else
-    starting_magn = nothing
-end
+Ham = Hamiltonian(atoms, pspots, ecutwfc, options);
 
 psiks = rand_BlochWavefunc(Ham);
 electrons_scf_G!(
@@ -19,5 +16,5 @@ electrons_scf_G!(
     psiks = psiks,
     NiterMax = 100,
     betamix = 0.1,
-    starting_magn = starting_magn
+    starting_magn = Ham.options.starting_magn
 )
